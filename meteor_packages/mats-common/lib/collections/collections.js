8d250c2395aafbd01fd6261daea1f56a3ba636ff
pierce <randy.pierce@noaa.gov>
2016-08-11 19:46:12 -0600

    add routes for apps

--- meteor_packages/mats-common/lib/collections/collections.js
+++ meteor_packages/mats-common/lib/collections/collections.js
@@ -37,6 +37,31 @@ Router.route('/', function () {
     });
 });
 
+Router.route('/wfip2', function () {
+    this.render('Home', {
+    });
+});
+
+Router.route('/upperair', function () {
+    this.render('Home', {
+    });
+});
+
+Router.route('/visibility', function () {
+    this.render('Home', {
+    });
+});
+
+Router.route('/surface', function () {
+    this.render('Home', {
+    });
+});
+
+Router.route('/ceiling', function () {
+    this.render('Home', {
+    });
+});
+
 
 InputTypes = {
     textInput : 'textInput',
