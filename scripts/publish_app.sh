f11eb92e748b2f89169a6a56893e65393fffa13f
pierce <randy.pierce@noaa.gov>
2016-11-11 09:52:16 -0700

    add comment about links

--- scripts/publish_app.sh
+++ scripts/publish_app.sh
@@ -26,6 +26,8 @@ fi
 rsync -ravl --rsh=ssh --delete  --include-from=/builds/buildArea/MATS_for_EMB/scripts/meteor_includes /web/  ${server}:
 rsync -ravl --rsh=ssh --delete  --include-from=/builds/buildArea/MATS_for_EMB/scripts/project_includes /web/*  ${server}:
 
-echo "do not forget to restart nginx on ${server}, and to check the links for node and npm that are in /usrlocal/bin"
-
+echo "do not forget to restart nginx on ${server}."
+echo "and check the links for node and npm that are in /usrlocal/bin. If the meteor install has changed (due to meteor upgrade), fix these links"
+echo "ln -sf /web/.meteor/packages/meteor-tool/.1.4.2_1.r4gde0++os.linux.x86_64+web.browser+web.cordova/mt-os.linux.x86_64/dev_bundle/bin/node /usr/local/bin/node"
+echo "ln -sf /web/.meteor/packages/meteor-tool/.1.4.2_1.r4gde0++os.linux.x86_64+web.browser+web.cordova/mt-os.linux.x86_64/dev_bundle/bin/npm /usr/local/bin/npm"
 exit 0
