1a9bd0f48b350daf771d6e32dd1a746193b4c762
pierce <randy.pierce@noaa.gov>
2017-01-06 09:41:06 -0700

    new nightly build script

---
+++ scripts/nightly_build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root- exiting" 2>&1
+   exit 1
+fi
+
+requestedApp="$1"
+
+cd /builds/buildArea
+
+su - www-data -c "./build_deploy_apps.shi ${requestedApp}" 2>&1
+
+service nginx restart 2 >&1
+
+exit 0
