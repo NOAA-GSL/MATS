49fa586aa91079bb7cf2e781c4d1d45912080d82
pierce <randy.pierce@noaa.gov>
2017-06-12 10:38:34 -0600

    build scripts

--- scripts/common/build_deploy_apps-int.sh
+++ scripts/common/build_deploy_apps-int.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /bin/bash MATS_for_EMB/scripts/common/build_meteorsrv-int.sh $1
 /bin/bash MATS_for_EMB/scripts/common/deploy_meteorsrv-int.sh $1
-/bin/bash MATS_for_EMB/scripts/common/build_applist-int.sh
+
