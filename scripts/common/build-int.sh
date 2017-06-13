c83eea1d501de93b082571636ef3bf4c79de3cdc
Kirk Holub <kirk.l.holub@noaa.gov>
2017-06-13 15:01:39 -0600

    build work

--- scripts/common/build-int.sh
+++ scripts/common/build-int.sh
@@ -13,8 +13,8 @@ export apps=""
 if [ -d MATS_for_EMB ]
 then
     cd MATS_for_EMB
-    git checkout master
-    apps=( $(git diff --name-only  origin | grep apps | cut -f2 -d'/') )
+    git fetch
+    apps=( $(git --no-pager diff master origin/master --name-only | grep apps | cut -f2 -d'/') )
     cd ..
 else
     unset apps
@@ -22,17 +22,17 @@ fi
 
 rm -rf MATS_for_EMB
 git clone gerrit:MATS_for_EMB
-git checkout master
+
 # NEED to implement git checkout tag
 cd MATS_for_EMB
-if [ "X" == "X${tag}" ]
+if [ "X" != "X${tag}" ]
 then
     # tag is requested so just build the app to the specified tag
     git checkout tags/${tag} -b master
     su - www-data -c "cd /builds/buildArea && /bin/bash /builds/buildArea/build_deploy_apps-int.sh" ${taggedApp} 2>&1
 fi
 
-if [ "X" == "X${apps}" ]
+if [ "X" != "X${apps}" ]
 then
     su - www-data -c "cd /builds/buildArea && /bin/bash /builds/buildArea/build_deploy_apps-int.sh" 2>&1
 else
