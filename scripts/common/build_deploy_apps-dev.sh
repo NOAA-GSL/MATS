77f5e6efb331b6a5bad213eca68b0176585ea5ec
pierce <randy.pierce@noaa.gov>
2017-07-05 12:03:55 -0600

    fix path

--- scripts/common/build_deploy_apps-dev.sh
+++ scripts/common/build_deploy_apps-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/bin/bash MATS_for_EMB/scripts/common/build_meteorsrv-dev.sh $1
-/bin/bash MATS_for_EMB/scripts/common/deploy_meteorsrv-dev.sh $1
+/bin/bash /builds/buildArea/MATS_for_EMB/scripts/common/build_meteorsrv-dev.sh $1
+/bin/bash /builds/buildArea/MATS_for_EMB/scripts/common/deploy_meteorsrv-dev.sh $1
 
