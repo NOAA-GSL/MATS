c1f8f4e27320e5984143ca284b9e5ecd02ae088b
pierce <randy.pierce@noaa.gov>
2017-07-05 11:59:35 -0600

    fix broken if

--- scripts/common/build-dev.sh
+++ scripts/common/build-dev.sh
@@ -8,13 +8,7 @@ fi
 requestedApp="$1"
 
 cd /builds/buildArea
-if [ -d MATS_for_EMB ]
 su - www-data -c "cd /builds/buildArea && /bin/bash /builds/buildArea/scripts/common/build_deploy_apps-dev.sh ${requestedApp}" 2>&1
-
 /bin/bash MATS_for_EMB/scripts/common/build_applist-dev.sh
-
 /bin/systemctl restart nginx 2>&1
-
-
-
 exit 0
