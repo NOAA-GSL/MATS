1b7895caafb734e54fa1890fca8e381156d1f3e3
pierce <randy.pierce@noaa.gov>
2017-09-15 15:53:47 -0600

    change the house and the 'home' to look like a clickable link

--- meteor_packages/mats-common/templates/topnav/top_nav.js
+++ meteor_packages/mats-common/templates/topnav/top_nav.js
@@ -3,8 +3,10 @@ Template.topNav.events({
         event.preventDefault();
         var matsref = Session.get("app").matsref;
         window.location.replace(matsref);
+        return false;
     },
     'click .about' : function() {
         $("#modal-display-about").modal();
+        return false;
     }
 });
