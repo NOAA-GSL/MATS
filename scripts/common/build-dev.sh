14a0f2d2b34070aa7c4e7ff65fcee3c48ae0adc1
pierce <randy.pierce@noaa.gov>
2017-10-05 14:48:39 -0600

    build modifications to use mongo utilities

--- scripts/common/build-dev.sh
+++ scripts/common/build-dev.sh
@@ -9,6 +9,15 @@ requestedApp="$1"
 
 cd /builds/buildArea
 su - www-data -c "cd /builds/buildArea && /bin/bash /builds/buildArea/MATS_for_EMB/scripts/common/build_deploy_apps-dev.sh ${requestedApp}" 2>&1
+if [ $? -ne 0 ]; then
+   echo $0  - /builds/buildArea/MATS_for_EMB/scripts/common/build_deploy_apps-dev.sh failed - exiting
+   exit 1
+fi
 su - www-data -c "cd /builds/buildArea && /bin/bash /builds/buildArea/MATS_for_EMB/scripts/common/build_applist-dev.sh" 2>&1
+if [ $? -ne 0 ]; then
+   echo $0  - /builds/buildArea/MATS_for_EMB/scripts/common/build_applist-dev.sh failed - exiting
+   exit 1
+fi
+echo restarting nginx
 /bin/systemctl restart nginx 2>&1
 exit 0
