ff618716ce3075077ef014756da351815bc814ec
pierce <randy.pierce@noaa.gov>
2017-10-05 13:44:40 -0600

    build modifications to use mongo utilities

--- scripts/common/build_meteorsrv-dev.sh
+++ scripts/common/build_meteorsrv-dev.sh
@@ -14,7 +14,6 @@ requestedApp="$1"
 
 echo "$0 ----------- started with args $*"
 date
-
 cd ${BUILD_DIRECTORY}
 if [ ! -d "${DEPLOYMENT_DIRECTORY}" ]; then
     echo "${DEPLOYMENT_DIRECTORY} does not exist,  clone ${DEPLOYMENT_DIRECTORY}"
@@ -27,12 +26,14 @@ if [ ! -d "${DEPLOYMENT_DIRECTORY}" ]; then
 fi
 
 cd ${DEPLOYMENT_DIRECTORY}
+echo checking out ${BUILD_CODE_BRANCH}
 /usr/bin/git checkout ${BUILD_CODE_BRANCH}
 if [ $? -ne 0 ]; then
     echo "failed to /usr/bin/git checkout ${BUILD_CODE_BRANCH} - must exit now"
     exit 1
 fi
 
+echo fetching
 /usr/bin/git fetch
 if [ $? -ne 0 ]; then
     echo "failed to /usr/bin/git fetch - must exit now"
@@ -40,17 +41,25 @@ if [ $? -ne 0 ]; then
 fi
 
 #build all of the apps that have changes (or if a meteor_package change just all the apps)
-
 buildableApps=($(getBuildableAppsForServer "${SERVER}"))
-diffs=$(/usr/bin/git diff -- no-pager --name-only origin/${BUILD_CODE_BRANCH})
+echo buildable apps are....
+echo ${buildableApps[*]}
+diffs=$(/usr/bin/git --no-pager diff --name-only origin/${BUILD_CODE_BRANCH} | grep -v 'appProductionStatus')
+if [ $? -ne 0 ]; then
+    echo "failed to /usr/bin/git diff - must exit now"
+    exit 1
+fi
 changedApps=($(echo ${diffs} | grep apps | cut -f2 -d'/'))
+echo changedApps are ${changedApps}
 meteor_package_changed=$(echo ${diffs}  | grep meteor_packages | cut -f2 -d'/')
+
 unset apps
 if [ "X${requestedApp}" != "X" ]; then
     apps+=(${requestedApp})
 else
     if [ "X${meteor_package_changed}" != "X"  ]; then
         # common code changed so we have to build all the apps
+        echo common code changed - must build all buildable apps
         apps=${buildableApps}
     else
         # no common code changes do just build apps
@@ -60,32 +69,37 @@ else
                 apps+=($a)
             fi
         done
+        echo modified and buildable apps are ${apps[*]}
     fi
 fi
 echo "$0 building these apps ${apps[*]}"
-
 for app in ${apps[*]}; do
     cd $app
     echo "$0 - building app $x"
     meteor reset
     meteor npm cache clean
     meteor npm install
-    meteor build /builds
     rollDevelopmentVersionAndDateForAppForServer ${app} ${SERVER}
-    # export the appProductionStatus collections file
+    echo export the appProductionStatus collections file
     exportCollections ${DEPLOYMENT_DIRECTORY}/appProductionStatusCollections
     /usr/bin/git commit -m"automated export" ${DEPLOYMENT_DIRECTORY}/appProductionStatusCollections
-    # copy deployment.json to uncontrolled common area
+    echo copy deployment.json to uncontrolled common area
     cat ${DEPLOYMENT_DIRECTORY}/appProductionStatusCollections/deployment.json |
         ${DEPLOYMENT_DIRECTORY}/scripts/common/makeCollectionExportValid.pl > ${DEPLOYMENT_DIRECTORY}/meteor_packages/mats-common/public/deployment/deployment.json
-    # make a tag
+    echo building app
+    meteor build /builds
+    if [ $? -ne 0 ]; then
+        echo "failed to meteor build - must exit now"
+        exit 1
+    fi
+    echo make a tag
     buildVer=$(getVersionForAppForServer ${app} ${SERVER})
     tag="${app}-${buildVer}"
     git tag ${tag}
     cd ..
 done
 
-#commit any tags that we added
+echo push tags that we added
 git push origin --tags
 # clean up /tmp files
 echo "cleaning up /tmp/npm-* files"
@@ -93,3 +107,4 @@ rm -rf /tmp/npm-*
 
 date
 echo "$0 ----------------- finished" 
+exit 0
