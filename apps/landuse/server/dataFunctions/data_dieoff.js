14d1aebd396ce713635e9eecc306ad56814fab40
Molly Smith <molly.b.smith@noaa.gov>
2017-11-30 10:35:01 -0700

    Added landuse app

---
+++ apps/landuse/server/dataFunctions/data_dieoff.js
@@ -0,0 +1,186 @@
+import {matsCollections} from 'meteor/randyp:mats-common';
+import {matsTypes} from 'meteor/randyp:mats-common';
+import {matsDataUtils} from 'meteor/randyp:mats-common';
+import {mysql} from 'meteor/pcel:mysql';
+import {moment} from 'meteor/momentjs:moment'
+
+dataDieOff = function (plotParams, plotFunction) {
+    var dataRequests = {}; // used to store data queries
+    var dataFoundForCurve = true;
+    var totalProecssingStart = moment();
+    var dateRange = matsDataUtils.getDateRange(plotParams.dates);
+    var fromDate = dateRange.fromDate;
+    var toDate = dateRange.toDate;
+    var fromSecs = dateRange.fromSeconds;
+    var toSecs = dateRange.toSeconds;
+    var error = "";
+    var curves = plotParams.curves;
+    var curvesLength = curves.length;
+    var dataset = [];
+    var axisMap = Object.create(null);
+    var xmax = Number.MIN_VALUE;
+    var ymax = Number.MIN_VALUE;
+    var xmin = Number.MAX_VALUE;
+    var ymin = Number.MAX_VALUE;
+
+    for (var curveIndex = 0; curveIndex < curvesLength; curveIndex++) {
+        var curve = curves[curveIndex];
+        var diffFrom = curve.diffFrom;
+        var model = matsCollections.CurveParams.findOne({name: 'data-source'}).optionsMap[curve['data-source']][0];
+        var vgtypStr = curve['vgtyp'];
+        var vgtyp = Object.keys(matsCollections.CurveParams.findOne({name: 'vgtyp'}).valuesMap).find(key => matsCollections.CurveParams.findOne({name: 'vgtyp'}).valuesMap[key] === vgtypStr);
+        var label = curve['label'];
+        var variableStr = curve['variable'];
+        var variableOptionsMap = matsCollections.CurveParams.findOne({name: 'variable'}, {optionsMap: 1})['optionsMap'];
+        var variable = variableOptionsMap[variableStr];
+        var statisticSelect = curve['statistic'];
+        var statisticOptionsMap = matsCollections.CurveParams.findOne({name: 'statistic'}, {optionsMap: 1})['optionsMap'];
+        var statistic;
+        if (variableStr == 'temperature' || variableStr == 'dewpoint' ) {
+            statistic = statisticOptionsMap[statisticSelect][0];
+        } else if (variableStr == 'wind'  ) {
+            statistic = statisticOptionsMap[statisticSelect][2];
+        } else {
+            statistic = statisticOptionsMap[statisticSelect][1];
+        }
+        statistic = statistic.replace(/\{\{variable0\}\}/g, variable[0]);
+        statistic = statistic.replace(/\{\{variable1\}\}/g, variable[1]);
+        var validTimes = curve['valid-time'] === undefined ? [] : curve['valid-time'];
+        const forecastLength = curve['dieoff-forecast-length'];
+        if (forecastLength !== "dieoff") {
+            throw new Error("INFO:  non dieoff curves are not yet supported");
+        }
+        // axisKey is used to determine which axis a curve should use.
+        // This axisKeySet object is used like a set and if a curve has the same
+        // variable and statistic (axisKey) it will use the same axis,
+        // The axis number is assigned to the axisKeySet value, which is the axisKey.
+        //CHANGED TO PLOT ON THE SAME AXIS IF SAME STATISTIC, REGARDLESS OF THRESHOLD
+        var axisKey = statisticSelect;
+        curves[curveIndex].axisKey = axisKey; // stash the axisKey to use it later for axis options
+        var interval = undefined;
+        var d = [];
+        if (diffFrom == null) {
+            var statement = "SELECT " +
+                "m0.fcst_len AS avtime, " +
+                "    Count(DISTINCT m0.valid_day + 3600 * m0.hour) AS N_times, " +
+                "    Count(m0.hour) / 1000                         AS Nhrs0, " +
+                "    {{statistic}} " +
+                "FROM {{model}} AS m0 " +
+                "WHERE 1 = 1 " +
+                "{{validTimeClause}} " +
+                "and m0.vgtyp IN({{vgtyp}}) "+
+                "and m0.valid_day+3600*m0.hour >= '{{fromSecs}}' " +
+                "and m0.valid_day+3600*m0.hour <= '{{toSecs}}' " +
+                "group by avtime " +
+                "order by avtime" +
+                ";";
+
+            statement = statement.replace('{{vgtyp}}', vgtyp);
+            statement = statement.replace('{{fromSecs}}', fromSecs);
+            statement = statement.replace('{{toSecs}}', toSecs);
+            statement = statement.replace('{{model}}', model +"_vgtyp");
+            statement = statement.replace('{{statistic}}', statistic);
+            var validTimeClause =" ";
+            if (validTimes.length > 0){
+                validTimeClause = " and  m0.hour IN(" + validTimes + ")";
+            }
+            statement = statement.replace('{{validTimeClause}}', validTimeClause);
+
+            if ((statisticSelect == 'MAE' && variableStr == 'wind') || (statisticSelect == 'MAE' && variableStr == 'RH') || (statisticSelect == 'Std deviation' && variableStr == 'wind')) {
+                throw new Error("INFO:  The statistic/variable combination [" + statisticSelect + " and " + variableStr + "] is not supported by the database.");
+            }
+
+            dataRequests[curve.label] = statement;
+            var queryResult;
+            var startMoment = moment();
+            var finishMoment;
+            try {
+                queryResult = matsDataUtils.queryDieoffDB(sumPool,statement, interval);
+                finishMoment = moment();
+                dataRequests["data retrieval (query) time - " + curve.label] = {
+                    begin: startMoment.format(),
+                    finish: finishMoment.format(),
+                    duration: moment.duration(finishMoment.diff(startMoment)).asSeconds() + " seconds",
+                    recordCount: queryResult.data.length
+                }
+                d = queryResult.data;
+            } catch (e) {
+                e.message = "Error in queryDB: " + e.message + " for statement: " + statement;
+                throw new Error(e.message);
+            }
+            if (queryResult.error !== undefined && queryResult.error !== "") {
+                if (queryResult.error === matsTypes.Messages.NO_DATA_FOUND) {
+                    // This is NOT an error just a no data condition
+                    dataFoundForCurve = false;
+                } else {
+                    error += "Error from verification query: <br>" + queryResult.error + "<br> query: <br>" + statement + "<br>";
+                    throw (new Error(error));
+                }
+            }
+
+            var postQueryStartMoment = moment();
+            if (dataFoundForCurve) {
+                xmin = xmin < d[0][0] ? xmin : d[0][0];
+                xmax = xmax > d[d.length - 1][0] ? xmax : d[d.length - 1][0];
+                var sum = 0;
+                var count = 0;
+                for (var i = 0; i < d.length; i++) {
+                    if (d[i][1] !== null) {
+                        sum = sum + d[i][1];
+                        count++;
+                        ymin = Number(ymin) < Number(d[i][1]) ? ymin : d[i][1];
+                        ymax = Number(ymax) > Number(d[i][1]) ? ymax : d[i][1];
+                    }
+                }
+            }
+        } else {
+            // this is a difference curve
+            const diffResult = matsDataUtils.getDataForSeriesDiffCurve({dataset:dataset, ymin:ymin, ymax:ymax, diffFrom:diffFrom});
+            d = diffResult.dataset;
+            ymin = diffResult.ymin;
+            ymax = diffResult.ymax;
+        }
+
+        const mean = sum / count;
+        const annotation = label + "- mean = " + mean.toPrecision(4);
+        curve['annotation'] = annotation;
+        curve['ymin'] = ymin;
+        curve['ymax'] = ymax;
+        curve['axisKey'] = axisKey;
+        const cOptions = matsDataUtils.generateSeriesCurveOptions(curve, curveIndex, axisMap, d);  // generate plot with data, curve annotation, axis labels, etc.
+        dataset.push(cOptions);
+        var postQueryFinishMoment = moment();
+        dataRequests["post data retreival (query) process time - " + curve.label] = {
+            begin: postQueryStartMoment.format(),
+            finish: postQueryFinishMoment.format(),
+            duration: moment.duration(postQueryFinishMoment.diff(postQueryStartMoment)).asSeconds() + ' seconds'
+        }
+    }  // end for curves
+
+    //if matching
+    if (curvesLength > 1 && (plotParams['plotAction'] === matsTypes.PlotActions.matched)) {
+        dataset = matsDataUtils.getDieOffMatchedDataSet(dataset);
+    }
+
+    // add black 0 line curve
+    // need to define the minimum and maximum x value for making the zero curve
+    dataset.push({color:'black',points:{show:false},annotation:"",data:[[xmin,0,"zero"],[xmax,0,"zero"]]});
+    const resultOptions = matsDataUtils.generateDieoffPlotOptions( dataset, curves, axisMap );
+    var totalProecssingFinish = moment();
+    dataRequests["total retrieval and processing time for curve set"] = {
+        begin: totalProecssingStart.format(),
+        finish: totalProecssingFinish.format(),
+        duration: moment.duration(totalProecssingFinish.diff(totalProecssingStart)).asSeconds() + ' seconds'
+    }
+
+    var result = {
+        error: error,
+        data: dataset,
+        options: resultOptions,
+        basis: {
+            plotParams: plotParams,
+            queries: dataRequests
+        }
+    };
+    plotFunction(result);
+};
