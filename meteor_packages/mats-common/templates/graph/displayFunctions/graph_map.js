f6fc5fca224ae2eb287d92d1e832fdd8dd9d66a3
jeffrey.a.hamilton <Jeffrey.A.Hamilton@noaa.gov>
2018-03-05 12:59:41 -0700

    adding function to create the map output

--- meteor_packages/mats-common/templates/graph/displayFunctions/graph_map.js
+++ meteor_packages/mats-common/templates/graph/displayFunctions/graph_map.js
@@ -2,22 +2,24 @@ graphMap = function(result) {
     var vpw = Math.min(document.documentElement.clientWidth, window.innerWidth || 0);
     var vph = Math.min(document.documentElement.clientHeight, window.innerHeight || 0);
     var min = Math.min(vpw,vph);
-    var dataset = result.data;
 
-    var defaultPoint = this.data.defaultMapView.point;
-    var defaultZoomLevel = this.data.defaultMapView.zoomLevel;
-    var minZoomLevel = this.data.defaultMapView.minZoomLevel;
-    var maxZoomLevel = this.data.defaultMapView.maxZoomLevel;
-    var peerName = this.data.peerName;
+    var dataset = result.data;
+    var options = result.options
+
+    var defaultPoint = [39.834, -98.604];
+    var defaultZoomLevel = 4;
+    var minZoomLevel = 3;
+    var maxZoomLevel = 10;
+    var peerName = dataset.sites.name;
 
     var targetElement = document.getElementsByName(peerName)[0];
     if (!targetElement) {
         return;
     }
     var targetId = '#' + targetElement.id;
-    var markers = this.data.optionsMap;   // from app startup
+    var markers = dataset.sites;   // from app startup and queries
     var markerFeatures = {};
-    var map = L.map(this.data.name + "-" + this.data.type, {
+    var map = L.map("stationMap-Map", {
             doubleClickZoom: false,
             scrollWheelZoom: false,
             trackResize:true,
@@ -46,14 +48,14 @@ graphMap = function(result) {
         markerFeatures = {};
     }
 
-    var createUnSelectedIcon = function (m) {
+    var createIcon = function (m, color) {
         var options = m.options;
         var icon = L.divIcon({
             iconSize: new L.point(0, 0),  // get rid of default white box icon
             html: '<div style="border: none;' +
             'width:' + options.size + 'px;' +
             'height:' + options.size + 'px;' +
-            'background-color:' + options.color + ';' +
+            'background-color:' + color + ';' +
             'border-radius:50%;">' +
             //'<b style="font-size: large">&nbsp;&nbsp;&nbsp;&nbsp;' + options.network + '</b>' +
             '</div>',
@@ -62,104 +64,57 @@ graphMap = function(result) {
         return icon;
     };
 
-    var createSelectedIcon = function (m) {
-        var options = m.options;
-        var icon = L.divIcon({
-            iconSize: new L.point(0, 0),  // get rid of default white box icon
-            html: '<div style="box-shadow: 0 0 0 ' + options.size / 2 + 'px ' + options.highLightColor + ';' +
-            'border: none;' +
-            'width:' + options.size + 'px;' +
-            'height:' + options.size + 'px;' +
-            'background-color:' + options.color + ';' +
-            'border-radius:50%;">' +
-            //'<b style="font-size: large"> &nbsp;&nbsp;&nbsp;&nbsp;' + options.network + '</b>' +
-            '</div>',
-            options: options
-        });
-        return icon;
-    };
-
-    var toggleTargetSelection = function (event) {
-        var marker = event.target;
-        var markerId = event.latlng.lat + ',' + event.latlng.lng + ':' + event.target.options.title;
-        var mFeatures = markerFeatures[markerId];
-        var icon = mFeatures.unSelectedIcon;
-        var peerOption = mFeatures.markerPeerOption;
-        var selectedValues = $(targetId).val() ? $(targetId).val() : [];
-        var index = selectedValues.indexOf(peerOption);
-        if (index > -1) {
-            // toggle off
-            icon = mFeatures.unSelectedIcon;
-            selectedValues.splice(index, 1);
-        } else {
-            //toggle on
-            icon = mFeatures.selectedIcon;
-            selectedValues.push(peerOption);
-        }
-        // set the selected value(s) of the peer
-        $(targetId).val(selectedValues);
-        // trigger the change event on the peer
-        $(targetId).trigger("change");
-        marker.setIcon(icon);
-    };
 
 
-
-    var refreshMarkersForPeer = function(peerElement) {
-        // find out what peer options are available
-        var peerOptions = [];
-        if (peerElement.options) {
-            for (var i = 0; i < peerElement.options.length; i++) {
-                peerOptions.push(peerElement.options[i].text);
+    var createMarkers = function() {
+        // clear the markers
+        map.eachLayer(function (l) {
+            if (l._icon) {
+                map.removeLayer(l);
             }
-
-            // clear the markers
-            map.eachLayer(function (l) {
-                if (l._icon) {
-                    map.removeLayer(l);
-                }
-            });
-        }
+        });
         for (var m = 0; m < markers.length; m++) {
             var markerPeerOption = markers[m].options.peerOption;
+            var markerData = dataset[m];
+            var markerStat = markerData[4];
+            if (markerStat < 0) {
+                var markerColor = "rgb(0,0,255)"
+            } else if (markerStat > 0) {
+                var markerColor = "rgb(255,0,0)"
+            } else {
+                var markerColor = "rgb(0,0,0)"
+            }
             if (_.contains(peerOptions, markerPeerOption)) {
-                var unSelectedIcon = createUnSelectedIcon(markers[m]);
-                var selectedIcon = createSelectedIcon(markers[m]);
+                var Icon = createIcon(markers[m], markerColor);
                 var markerOptions = markers[m].options;
                 var title = markerOptions.peerOption + ' - ' + markerOptions.title;
                 var point = markers[m].point;
                 var markerId = point[0] + ',' + point[1] + ':' + title;
                 var features = {
-                    unSelectedIcon: unSelectedIcon,
-                    selectedIcon: selectedIcon,
+                    Icon: Icon,
                     markerOptions: markerOptions,
                     markerPeerOption: markerPeerOption
                 };
 
                 var marker = new L.Marker(markers[m].point, {
-                    icon: unSelectedIcon,
+                    icon: Icon,
                     title: markers[m].options.peerOption + ' - ' + markers[m].options.title,
-                }).on('click', function (event) {
-                        // toggle selection of corresponding site option for this marker
-                        toggleTargetSelection(event);
-                    }
-                );
+                })//.on('click', function (event) {
+                //        // toggle selection of corresponding site option for this marker
+                //        toggleTargetSelection(event);
+                //    }
+                //);
                 markerFeatures[markerId] = features;
                 map.addLayer(marker);
             }
         }
     };
 
-    var refresh = function (peerElement) {
-        if (!peerElement) {
-            return;
-        }
-        var peerId = peerElement.id;
-        refreshMarkersForPeer(peerElement);
+    var refresh = function () {
+        createMarkers();
         var selectedValues = $('#' + peerId).val() ? $('#' + peerId).val() : [];
         // iterate through all the makers,
-        // set the selectedIcon if they are selected in the peer
-        // set the unSelectedIcon if they are not selected in the peer
+        // set the Icon
         $.each(map._layers, function (ml) {
             if (map._layers[ml]._latlng) {
                 var lat = map._layers[ml]._latlng.lat;
@@ -169,14 +124,10 @@ graphMap = function(result) {
                     return obj.point[0] === point[0] && obj.point[1] === point[1];
                 })[0];
                 if (marker !== undefined) {
-                    var peerOption = marker.options.peerOption;
                     var markerId = marker.point[0] + ',' + marker.point[1] + ':' + marker.options.peerOption + ' - ' + marker.options.title;
                     var mFeatures = markerFeatures[markerId];
-                    if (_.contains(selectedValues, peerOption)) {
-                        map._layers[ml].setIcon(mFeatures.selectedIcon);
-                    } else {
-                        map._layers[ml].setIcon(mFeatures.unSelectedIcon);
-                    }
+                    map._layers[ml].setIcon(mFeatures.Icon);
+
                 }
             }
         });
@@ -199,18 +150,18 @@ graphMap = function(result) {
     // initial resize seems to be necessary
     resizeMap(this);
     // register an event listener so that the item.js can ask the map div to resize after the map div becomes visible
-    var ref = this.data.name + '-' + this.data.type;
+    var ref = 'stationMap-Map';
     var elem = document.getElementById(ref);
     elem.addEventListener('resizeMap', function (e) {
         resizeMap(e.detail);
     });
 
     // register an event listener so that the select.js can ask the map div to refresh after a selection
-    var ref = this.data.name + '-' + this.data.type;
+    var ref = 'stationMap-Map';
     var elem = document.getElementById(ref);
     elem.addEventListener('refresh', function (e) {
         refresh(e.detail.refElement);
     });
-    refresh(targetElement);
+    refresh();
 
 };
