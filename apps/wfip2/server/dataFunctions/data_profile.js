37478c92485ef7d1fe145826e15384ec4de648ac
pierce <randy.pierce@noaa.gov>
2018-03-09 14:27:59 -0700

    fix instrument NaN interpolation problem (Joes test case)

--- apps/wfip2/server/dataFunctions/data_profile.js
+++ apps/wfip2/server/dataFunctions/data_profile.js
@@ -7,7 +7,6 @@ import {moment} from 'meteor/momentjs:moment';
 
 
 dataProfile = function (plotParams, plotFunction) {
-
     //console.log("plotParams: ", JSON.stringify(plotParams, null, 2));
     var dataRequests = {}; // used to store data queries
     var totalProecssingStart = moment();
