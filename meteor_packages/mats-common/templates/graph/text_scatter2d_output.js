e69086743ffa8f0f57f8e41661033618feddd561
randy.pierce <randy.pierce@noaa.gov>
2018-09-14 14:41:13 -0600

    in progress refactoring for hash stuff Result collection savedresults refactoring Server side working client plots graph, text for series works.

--- meteor_packages/mats-common/templates/graph/text_scatter2d_output.js
+++ meteor_packages/mats-common/templates/graph/text_scatter2d_output.js
@@ -1,45 +1,36 @@
-import { matsCollections } from 'meteor/randyp:mats-common';
-import { matsCurveUtils } from 'meteor/randyp:mats-common';
-import { matsTypes } from 'meteor/randyp:mats-common';
-import { moment } from 'meteor/momentjs:moment';
-import { matsPlotUtils } from 'meteor/randyp:mats-common';
+import {matsCollections, matsCurveUtils, matsPlotUtils, matsTypes} from 'meteor/randyp:mats-common';
+import {moment} from 'meteor/momentjs:moment';
+/*
+Referring to the Session variable plotResultKey here causes the html template to get re-rendered with the current graph data
+(which is in the Results collection).
+ */
+
+var times = [];
+const getDataForTime = function (data, time) {
+    for (var i = 0; i < data.length; i++) {
+        if (data[i][0] == Number(time)) {
+            return data[i] === null ? undefined : data[i];
+        }
+    }
+    return undefined;
+};
 
 Template.textScatter2dOutput.helpers({
     plotName: function() {
         return Session.get('plotName');
     },
     curves: function () {
-        /*
-         This (plotResultsUpDated) is very important.
-         The page is rendered whe the graph page comes up, but the data from the data processing callback
-         in plotList.js or curveList.js may not have set the global variable
-         PlotResult. The callback sets the variable then sets the session variable plotResultsUpDated.
-         Referring to plotResultsUpDated here causes the html to get re-rendered with the current graph data
-         (which is in the PlotResults global). This didn't used to be necessary because the plot data
-         was contained in the session, but some unknown ddp behaviour having to do with the amount of plot data
-         made that unworkable.
-         */
-        var plotResultsUpDated = Session.get('PlotResultsUpDated');
-        if (plotResultsUpDated === undefined) {
-            return [];
-        }
+        Session.get("plotResultKey"); // make sure we re-render when data changes
         return Session.get('Curves');
     },
     curveText: function () {
-        if (this.regionName) {
-            this.regionName = this.region.split(' ')[0];
-        }  // regionName might be needed in getCurveText but only region is defined
         var text = matsPlotUtils.getCurveText(matsPlotUtils.getPlotType(),this);
         return text;
     },
     headers: function(curve) {
         var bFitLabel = "best fit";
-        var plotResultsUpDated = Session.get('PlotResultsUpDated');
-
-        if (plotResultsUpDated !== undefined) {
-            if (matsCurveUtils.PlotResult.length >1) {
+        if (Session.get("plotResultKey") !== undefined) {
                 bFitLabel = "best fit";
-            }
         }
         var str = "<th>" + curve.label + " x axis</th>" +
             "<th>" + curve.label + " y axis </th>" +
@@ -47,21 +38,10 @@ Template.textScatter2dOutput.helpers({
         return str;
     },
     dataRows: function(curve) {
-        /*
-         This (plotResultsUpDated) is very important.
-         The page is rendered whe the graph page comes up, but the data from the data processing callback
-         in plotList.js or curveList.js may not have set the global variable
-         PlotResult. The callback sets the variable then sets the session variable plotResultsUpDated.
-         Referring to plotResultsUpDated here causes the html to get re-rendered with the current graph data
-         (which is in the PlotResults global). This didn't used to be necessary because the plot data
-         was contained in the session, but some unknown ddp behaviour having to do with the amount of plot data
-         made that unworkable.
-         */
-        var plotResultsUpDated = Session.get('PlotResultsUpDated');
-        if (plotResultsUpDated === undefined) {
+        if (matsPlotUtils.getPlotType() != matsTypes.PlotTypes.scatter2d) {
             return [];
         }
-        if (matsPlotUtils.getPlotType() != matsTypes.PlotTypes.scatter2d) {
+        if (Session.get("plotResultKey") === undefined) {
             return [];
         }
 
@@ -71,23 +51,19 @@ Template.textScatter2dOutput.helpers({
                 break;
             }
         }
-        if (matsCurveUtils.PlotResult.data === undefined) {
-            return [];
-        }
-        var dataRows = _.range(matsCurveUtils.PlotResult.data[i].data.length);
+        var plotResultData = matsCollections.Results.findOne({key: Session.get("plotResultKey")}).result.data;
+        var dataRows = _.range(plotResultData[i].data.length);
         return dataRows;
     },
     points: function(curve, rowIndex) {
-        if (matsCurveUtils.PlotResult.data === undefined) {
-            return "";
-        }
+        var plotResultData = matsCollections.Results.findOne({key: Session.get("plotResultKey")}).result.data;
         var line = '';
-        for (var i = 0; i < matsCurveUtils.PlotResult.data.length; i++) {
-            if (matsCurveUtils.PlotResult.data[i].label == curve.label) {
-                line += "<td>" + Number(matsCurveUtils.PlotResult.data[i].data[rowIndex][0]).toPrecision(4) + "</td> <td>" + Number(matsCurveUtils.PlotResult.data[i].data[rowIndex][1]).toPrecision(4) + "</td>";
+        for (var i = 0; i < plotResultData.length; i++) {
+            if (plotResultData[i].label == curve.label) {
+                line += "<td>" + Number(plotResultData[i].data[rowIndex][0]).toPrecision(4) + "</td> <td>" + Number(plotResultData[i].data[rowIndex][1]).toPrecision(4) + "</td>";
             }
-            if (matsCurveUtils.PlotResult.data[i].label && matsCurveUtils.PlotResult.data[i].label.search(curve.label + '-best fit') > -1 && line.slice(4, 4 + Number(matsCurveUtils.PlotResult.data[i].data[rowIndex][0]).toPrecision(4).length) == Number(matsCurveUtils.PlotResult.data[i].data[rowIndex][0]).toPrecision(4)) {
-                line += "</td> <td>" + Number(matsCurveUtils.PlotResult.data[i].data[rowIndex][1]).toPrecision(4) + "</td>"
+            if (plotResultData[i].label && plotResultData[i].label.search(curve.label + '-best fit') > -1 && line.slice(4, 4 + Number(plotResultData[i].data[rowIndex][0]).toPrecision(4).length) == Number(plotResultData[i].data[rowIndex][0]).toPrecision(4)) {
+                line += "</td> <td>" + Number(plotResultData[i].data[rowIndex][1]).toPrecision(4) + "</td>"
             }
         }
         return line;
@@ -111,21 +87,22 @@ Template.textScatter2dOutput.events({
             clabels += "," + curves[c].label;
         }
         data.push(clabels);
-        var curveNums = matsCurveUtils.PlotResult.data.length - 1;
-        var dataRows = _.range(matsCurveUtils.PlotResult.data[0].data.length);
+        var plotResultData = matsCollections.Results.findOne({key: Session.get("plotResultKey")}).result.data;
+        var curveNums = plotResultData.length - 1;
+        var dataRows = _.range(plotResultData[0].data.length);
         for (var rowIndex = 0; rowIndex < dataRows.length; rowIndex ++) {
-            var line = moment.utc(Number(matsCurveUtils.PlotResult.data[0].data[rowIndex][0])).format('YYYY-MM-DD:HH');
+            var line = moment.utc(Number(plotResultData[0].data[rowIndex][0])).format('YYYY-MM-DD:HH');
             for (var curveIndex = 0; curveIndex < curveNums; curveIndex++) {
-                var pdata = matsCurveUtils.PlotResult.data[curveIndex].data[rowIndex][1] !== null?(Number(matsCurveUtils.PlotResult.data[curveIndex].data[rowIndex][1])).toPrecision(4):fillStr;
+                var pdata = plotResultData[curveIndex].data[rowIndex][1] !== null?(Number(plotResultData[curveIndex].data[rowIndex][1])).toPrecision(4):fillStr;
                 line += "," + pdata;
             }
             data.push(line);
         }
-        var csvString = data.join("%0A");
-        var a         = document.createElement('a');
-        a.href        = 'data:attachment/csv,' + csvString;
-        a.target      = '_blank';
-        a.download    = 'data.csv';
+        const csvString = data.join("%0A");
+        const a = document.createElement('a');
+        a.href = 'data:attachment/csv,' + csvString;
+        a.target = '_blank';
+        a.download = 'data.csv';
         document.body.appendChild(a);
         a.click();
     }
