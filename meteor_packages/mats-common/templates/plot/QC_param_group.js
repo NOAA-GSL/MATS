a1eee3989e47df7e23d42531cef41daec1687fb9
Molly Smith <molly.b.smith@noaa.gov>
2018-12-03 15:35:40 -0700

    ACC plots now have different QC parameters

--- meteor_packages/mats-common/templates/plot/QC_param_group.js
+++ meteor_packages/mats-common/templates/plot/QC_param_group.js
@@ -1,7 +1,19 @@
 import { matsCollections } from 'meteor/randyp:mats-common';
+import { matsParamUtils } from 'meteor/randyp:mats-common';
 import { plotParamHandler } from 'meteor/randyp:mats-common';
 Template.QCParamGroup.helpers({
-
+    completenessNumber: function () {
+        var appName = matsParamUtils.getAppName();
+        if (appName === 'anomalycor') {
+            return '0';
+        } else {
+            return '75';
+        }
+    },
+    isACC: function () {
+        var appName = matsParamUtils.getAppName();
+        return appName === 'anomalycor'
+    }
 });
 
 Template.QCParamGroup.events({
