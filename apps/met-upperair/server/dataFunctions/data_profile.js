a4c8d9b2daf6f61a15325a5c53cef53eaf27704d
Molly Smith <molly.b.smith@noaa.gov>
2019-02-08 17:27:26 -0700

    python queries working for profiles, dieoffs, and validtimes

--- apps/met-upperair/server/dataFunctions/data_profile.js
+++ apps/met-upperair/server/dataFunctions/data_profile.js
@@ -6,7 +6,8 @@ import {matsDataDiffUtils} from 'meteor/randyp:mats-common';
 import {matsDataCurveOpsUtils} from 'meteor/randyp:mats-common';
 import {matsDataProcessUtils} from 'meteor/randyp:mats-common';
 import {mysql} from 'meteor/pcel:mysql';
-import {moment} from 'meteor/momentjs:moment'
+import {moment} from 'meteor/momentjs:moment';
+import {PythonShell} from 'python-shell';
 
 dataProfile = function (plotParams, plotFunction) {
     // initialize variables common to all curves
@@ -36,17 +37,34 @@ dataProfile = function (plotParams, plotFunction) {
         const label = curve['label'];
         const database = curve['database'];
         const model = matsCollections.CurveParams.findOne({name: 'data-source'}).optionsMap[database][curve['data-source']][0];
-        const region = curve['region'];
+        var regions_raw = curve['region'] === undefined ? [] : curve['region'];
+        var regionsClause = "";
+        if (regions_raw.length > 0) {
+            const regions = regions_raw.map(function (r) {
+                return "'" + r + "'";
+            }).join(',');
+            regionsClause = "and h.vx_mask IN(" + regions + ")";
+        }
         const variable = curve['variable'];
-        const statisticStr = curve['statistic'];
-        const statisticOptionsMap = matsCollections.CurveParams.findOne({name: 'statistic'}, {optionsMap: 1})['optionsMap'];
-        const statistic = statisticOptionsMap[statisticStr][0];
-        const forecastLengthStr = curve['forecast-length'];
-        const forecastValueMap = matsCollections.CurveParams.findOne({name: 'forecast-length'}, {valuesMap: 1})['valuesMap'][database][model];
-        const forecastLength = forecastValueMap[forecastLengthStr];
-        var vts = curve['valid-time'] === undefined ? [] : curve['valid-time'];
+        const statistic = curve['statistic'];
+        // the forecast lengths appear to have sometimes been inconsistent (by format) in the database so they
+        // have been sanitized for display purposes in the forecastValueMap.
+        // now we have to go get the damn ole unsanitary ones for the database.
+        var forecastLengthsClause = "";
+        var fcsts_raw = curve['forecast-length'] === undefined ? [] : curve['forecast-length'];
+        if (fcsts_raw.length > 0 ) {
+            const forecastValueMap = matsCollections.CurveParams.findOne({name: 'forecast-length'}, {valuesMap: 1})['valuesMap'][database][model];
+            const forecastLengths = fcsts_raw.map(function (fl) {
+                return forecastValueMap[fl];
+            }).join(',');
+            forecastLengthsClause = "and ld.fcst_lead IN (" + forecastLengths + ")";
+        }
+        var vts_raw = curve['valid-time'] === undefined ? [] : curve['valid-time'];
         var validTimeClause = "";
-        if (vts.length > 0) {
+        if (vts_raw.length > 0) {
+            const vts = vts_raw.map(function (vt) {
+                return "'" + vt + "'";
+            }).join(',');
             validTimeClause = "and floor(unix_timestamp(ld.fcst_valid_beg)%(24*3600)/3600) IN(" + vts + ")";
         }
         var dateRange = matsDataUtils.getDateRange(curve['curve-dates']);
@@ -54,9 +72,9 @@ dataProfile = function (plotParams, plotFunction) {
         var toSecs = dateRange.toSeconds;
         // axisKey is used to determine which axis a curve should use.
         // This axisKeySet object is used like a set and if a curve has the same
-        // units (axisKey) it will use the same axis.
+        // variable (axisKey) it will use the same axis.
         // The axis number is assigned to the axisKeySet value, which is the axisKey.
-        var axisKey = statisticStr;
+        var axisKey = variable;
         curves[curveIndex].axisKey = axisKey; // stash the axisKey to use it later for axis options
 
         var d;
@@ -67,49 +85,82 @@ dataProfile = function (plotParams, plotFunction) {
                 "count(distinct unix_timestamp(ld.fcst_valid_beg)) as N_times, " +
                 "min(unix_timestamp(ld.fcst_valid_beg)) as min_secs, " +
                 "max(unix_timestamp(ld.fcst_valid_beg)) as max_secs, " +
-                "{{statistic}} " +
+                "sum(ld.total) as N0, " +
+                "avg(ld.fbar) as fbar, " +
+                "avg(ld.obar) as obar, " +
+                "group_concat(ld.fbar order by unix_timestamp(ld.fcst_valid_beg), h.fcst_lev) as sub_fbar, " +
+                "group_concat(ld.obar order by unix_timestamp(ld.fcst_valid_beg), h.fcst_lev) as sub_obar, " +
+                "group_concat(ld.ffbar order by unix_timestamp(ld.fcst_valid_beg), h.fcst_lev) as sub_ffbar, " +
+                "group_concat(ld.oobar order by unix_timestamp(ld.fcst_valid_beg), h.fcst_lev) as sub_oobar, " +
+                "group_concat(ld.fobar order by unix_timestamp(ld.fcst_valid_beg), h.fcst_lev) as sub_fobar, " +
+                "group_concat(ld.total order by unix_timestamp(ld.fcst_valid_beg), h.fcst_lev) as sub_total, " +
+                "group_concat(unix_timestamp(ld.fcst_valid_beg) order by unix_timestamp(ld.fcst_valid_beg), h.fcst_lev) as sub_secs, " +
+                "group_concat(h.fcst_lev order by unix_timestamp(ld.fcst_valid_beg), h.fcst_lev) as sub_levs " +
                 "from {{database}}.stat_header h, " +
                 "{{database}}.line_data_sl1l2 ld " +
                 "where 1=1 " +
                 "and h.model = '{{model}}' " +
-                "and h.vx_mask in ({{region}}) " +
+                "{{regionsClause}} " +
                 "and unix_timestamp(ld.fcst_valid_beg) >= '{{fromSecs}}' " +
                 "and unix_timestamp(ld.fcst_valid_beg) <= '{{toSecs}}' " +
                 "{{validTimeClause}} " +
-                "and ld.fcst_lead = '{{forecastLength}}' " +
+                "{{forecastLengthsClause}} " +
                 "and h.fcst_var = '{{variable}}' " +
                 "and ld.stat_header_id = h.stat_header_id " +
                 "group by avVal " +
                 "order by avVal" +
                 ";";
 
-            statement = statement.replace('{{statistic}}', statistic);
             statement = statement.replace('{{database}}', database);
             statement = statement.replace('{{database}}', database);
             statement = statement.replace('{{model}}', model);
-            statement = statement.replace('{{region}}', region);
+            statement = statement.replace('{{regionsClause}}', regionsClause);
             statement = statement.replace('{{fromSecs}}', fromSecs);
             statement = statement.replace('{{toSecs}}', toSecs);
             statement = statement.replace('{{validTimeClause}}', validTimeClause);
-            statement = statement.replace('{{forecastLength}}', forecastLength);
+            statement = statement.replace('{{forecastLengthsClause}}', forecastLengthsClause);
             statement = statement.replace('{{variable}}', variable);
             dataRequests[curve.label] = statement;
+
+            const QCParams = matsDataUtils.getPlotParamsFromStack();
+            const completenessQCParam = Number(QCParams["completeness"]) / 100;
 
             var queryResult;
             var startMoment = moment();
             var finishMoment;
             try {
-                // send the query statement to the query function
-                queryResult = matsDataQueryUtils.queryDBSpecialtyCurve(sumPool, statement, plotType, hasLevels);
+                // send the query statement to the python query function
+                const pyOptions = {
+                    mode: 'text',
+                    pythonPath: '/Users/molly.b.smith/anaconda/bin/python',
+                    pythonOptions: ['-u'], // get print results in real-time
+                    scriptPath: process.env.METEOR_PACKAGE_DIRS + '/mats-common/private/',
+                    args: [statement, statistic, plotType, hasLevels, completenessQCParam]
+                };
+                var pyError = null;
+                const Future = require('fibers/future');
+                var future = new Future();
+                PythonShell.run('python_query_util.py', pyOptions, function (err, results) {
+                    if (err) {
+                        pyError = err;
+                        future["return"]();
+                    };
+                    queryResult = JSON.parse(results);
+                    // get the data back from the query
+                    d = queryResult.data;
                 finishMoment = moment();
                 dataRequests["data retrieval (query) time - " + curve.label] = {
                     begin: startMoment.format(),
                     finish: finishMoment.format(),
                     duration: moment.duration(finishMoment.diff(startMoment)).asSeconds() + " seconds",
-                    recordCount: queryResult.data.length
+                        recordCount: queryResult.data.x.length
                 };
-                // get the data back from the query
-                d = queryResult.data;
+                    future["return"]();
+                });
+                future.wait();
+                if (pyError != null) {
+                    throw new Error(pyError);
+                }
             } catch (e) {
                 // this is an error produced by a bug in the query function, not an error returned by the mysql database
                 e.message = "Error in queryDB: " + e.message + " for statement: " + statement;
@@ -123,7 +174,7 @@ dataProfile = function (plotParams, plotFunction) {
                     // this is an error returned by the mysql database
                     error += "Error from verification query: <br>" + queryResult.error + "<br> query: <br>" + statement + "<br>";
                     if (error.includes('Unknown column')) {
-                        throw new Error("INFO:  The statistic/variable combination [" + statisticStr + " and " + variable + "] is not supported by the database for the model/region [" + model + " and " + region + "].");
+                        throw new Error("INFO:  The statistic/variable combination [" + statistic + " and " + variable + "] is not supported by the database for the model/region [" + model + " and " + region + "].");
                     } else {
                         throw new Error(error);
                     }
@@ -136,8 +187,6 @@ dataProfile = function (plotParams, plotFunction) {
         } else {
             // this is a difference curve
             const diffResult = matsDataDiffUtils.getDataForDiffCurve(dataset, diffFrom, plotType, hasLevels);
-
-            // adjust axis stats based on new data from diff curve
             d = diffResult.dataset;
         }
 
@@ -146,8 +195,8 @@ dataProfile = function (plotParams, plotFunction) {
 
         // set curve annotation to be the curve mean -- may be recalculated later
         // also pass previously calculated axis stats to curve options
-        // profile plots always go from 0 to 1000 initially
-        curve['annotation'] = "";
+        const annotation = label + "- mean = " + mean.toPrecision(4);
+        curve['annotation'] = annotation;
         curve['xmin'] = d.xmin;
         curve['xmax'] = d.xmax;
         curve['ymin'] = ymin;
