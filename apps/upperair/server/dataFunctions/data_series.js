18ad1a0f47f12e28c8292a2af054c56cb44ddf54
Molly Smith <molly.b.smith@noaa.gov>
2019-02-12 16:24:51 -0700

    Fixed bug where model cadence wasn't considering valid time

--- apps/upperair/server/dataFunctions/data_series.js
+++ apps/upperair/server/dataFunctions/data_series.js
@@ -120,7 +120,7 @@ dataSeries = function (plotParams, plotFunction) {
             var finishMoment;
             try {
                 // send the query statement to the query function
-                queryResult = matsDataQueryUtils.queryDBTimeSeries(sumPool, statement, averageStr, model, forecastLength, fromSecs, toSecs, hasLevels, true);
+                queryResult = matsDataQueryUtils.queryDBTimeSeries(sumPool, statement, model, forecastLength, fromSecs, toSecs, averageStr, [], hasLevels, true);
                 finishMoment = moment();
                 dataRequests["data retrieval (query) time - " + curve.label] = {
                     begin: startMoment.format(),
