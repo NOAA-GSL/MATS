c46939e03552acb4f9cd09186781c92e05ddae7a
randy.pierce <randy.pierce@noaa.gov>
2019-03-25 17:15:44 -0600

    working on home button in topnav template

--- meteor_packages/mats-common/templates/topnav/top_nav.js
+++ meteor_packages/mats-common/templates/topnav/top_nav.js
@@ -4,9 +4,16 @@ Template.topNav.events({
         event.preventDefault();
         var homeref = document.referrer;
         if (homeref === "" || homeref === undefined) {
-            var r = document.loaction.href;
-            homeref = r.split("://")[0] + "://" + r.split("://")[1].split(".").splice(1,10).join(".")
+            var r = document.location.href;
+            if (r.split(":").length >= 2) {
+                // has a port - don't change it
+                homeref = r;
+            } else {
+                // doesn't have a port - strip the appreference
+                homeref = r.split("://")[0] + "://" + r.split("://")[1].split(".").splice(1, 10).join(".")
+            }
         }
+
         window.location.replace(homeref);
         return false;
     },
