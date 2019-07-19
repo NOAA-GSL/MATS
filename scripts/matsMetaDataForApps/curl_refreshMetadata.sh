0ee20df005318bbd429b85f920dfee3c3cc54c49
Molly Smith <molly.b.smith@noaa.gov>
2019-07-19 15:19:05 -0600

    added refreshMetadata bash script

---
+++ scripts/matsMetaDataForApps/curl_refreshMetadata.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# call refreshMetadata routes
+echo "Refreshing metadata for aircraft app at https://www.esrl.noaa.gov/gsd/mats/aircraft/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/aircraft/refreshMetadata
+echo "Refreshing metadata for anomalycor app at https://www.esrl.noaa.gov/gsd/mats/anomalycor/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/anomalycor/refreshMetadata
+echo "Refreshing metadata for ceiling app at https://www.esrl.noaa.gov/gsd/mats/ceiling/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/ceiling/refreshMetadata
+echo "Refreshing metadata for ceiling15 app at https://www.esrl.noaa.gov/gsd/mats/ceiling15/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/ceiling15/refreshMetadata
+echo "Refreshing metadata for compositeReflectivity app at https://www.esrl.noaa.gov/gsd/mats/compositeReflectivity/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/compositeReflectivity/refreshMetadata
+echo "Refreshing metadata for echotop app at https://www.esrl.noaa.gov/gsd/mats/echotop/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/echotop/refreshMetadata
+echo "Refreshing metadata for landuse app at https://www.esrl.noaa.gov/gsd/mats/landuse/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/landuse/refreshMetadata
+echo "Refreshing metadata for precipAQPI app at https://www.esrl.noaa.gov/gsd/mats/precipAQPI/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/precipAQPI/refreshMetadata
+echo "Refreshing metadata for precipitation1hr app at https://www.esrl.noaa.gov/gsd/mats/precipitation1hr/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/precipitation1hr/refreshMetadata
+echo "Refreshing metadata for precipitation24hr app at https://www.esrl.noaa.gov/gsd/mats/precipitation24hr/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/precipitation24hr/refreshMetadata
+echo "Refreshing metadata for precipitationSub24hr app at https://www.esrl.noaa.gov/gsd/mats/precipitationSub24hr/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/precipitationSub24hr/refreshMetadata
+echo "Refreshing metadata for surface app at https://www.esrl.noaa.gov/gsd/mats/surface/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/surface/refreshMetadata
+echo "Refreshing metadata for surfrad app at https://www.esrl.noaa.gov/gsd/mats/surfrad/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/surfrad/refreshMetadata
+echo "Refreshing metadata for upperair app at https://www.esrl.noaa.gov/gsd/mats/upperair/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/upperair/refreshMetadata
+echo "Refreshing metadata for vil app at https://www.esrl.noaa.gov/gsd/mats/vil/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/vil/refreshMetadata
+echo "Refreshing metadata for visibility app at https://www.esrl.noaa.gov/gsd/mats/visibility/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/visibility/refreshMetadata
+echo "Refreshing metadata for visibility15 app at https://www.esrl.noaa.gov/gsd/mats/visibility15/refreshMetadata"
+curl -k https://www.esrl.noaa.gov/gsd/mats/visibility15/refreshMetadata
