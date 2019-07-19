1f6f83f12c02f2589c2d4e617a134ba25a62606a
Molly Smith <molly.b.smith@noaa.gov>
2019-07-19 16:08:53 -0600

    updated curl refresh_metadata script

--- scripts/matsMetaDataForApps/curl_refreshMetadata.sh
+++ scripts/matsMetaDataForApps/curl_refreshMetadata.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
 
 # call refreshMetadata routes
-echo "Refreshing metadata for aircraft app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/aircraft/refreshMetadata"
+echo -e "\nRefreshing metadata for aircraft app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/aircraft/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/aircraft/refreshMetadata
-echo "Refreshing metadata for anomalycor app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/anomalycor/refreshMetadata"
+echo -e "\nRefreshing metadata for anomalycor app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/anomalycor/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/anomalycor/refreshMetadata
-echo "Refreshing metadata for ceiling app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/ceiling/refreshMetadata"
+echo -e "\nRefreshing metadata for ceiling app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/ceiling/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/ceiling/refreshMetadata
-echo "Refreshing metadata for ceiling15 app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/ceiling15/refreshMetadata"
+echo -e "\nRefreshing metadata for ceiling15 app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/ceiling15/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/ceiling15/refreshMetadata
-echo "Refreshing metadata for compositeReflectivity app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/compositeReflectivity/refreshMetadata"
+echo -e "\nRefreshing metadata for compositeReflectivity app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/compositeReflectivity/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/compositeReflectivity/refreshMetadata
-echo "Refreshing metadata for echotop app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/echotop/refreshMetadata"
+echo -e "\nRefreshing metadata for echotop app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/echotop/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/echotop/refreshMetadata
-echo "Refreshing metadata for landuse app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/landuse/refreshMetadata"
+echo -e "\nRefreshing metadata for landuse app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/landuse/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/landuse/refreshMetadata
-echo "Refreshing metadata for precipAQPI app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipAQPI/refreshMetadata"
+echo -e "\nRefreshing metadata for precipAQPI app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipAQPI/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipAQPI/refreshMetadata
-echo "Refreshing metadata for precipitation1hr app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipitation1hr/refreshMetadata"
+echo -e "\nRefreshing metadata for precipitation1hr app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipitation1hr/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipitation1hr/refreshMetadata
-echo "Refreshing metadata for precipitation24hr app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipitation24hr/refreshMetadata"
+echo -e "\nRefreshing metadata for precipitation24hr app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipitation24hr/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipitation24hr/refreshMetadata
-echo "Refreshing metadata for precipitationSub24hr app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipitationSub24hr/refreshMetadata"
+echo -e "\nRefreshing metadata for precipitationSub24hr app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipitationSub24hr/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/precipitationSub24hr/refreshMetadata
-echo "Refreshing metadata for surface app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/surface/refreshMetadata"
+echo -e "\nRefreshing metadata for surface app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/surface/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/surface/refreshMetadata
-echo "Refreshing metadata for surfrad app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/surfrad/refreshMetadata"
+echo -e "\nRefreshing metadata for surfrad app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/surfrad/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/surfrad/refreshMetadata
-echo "Refreshing metadata for upperair app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/upperair/refreshMetadata"
+echo -e "\nRefreshing metadata for upperair app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/upperair/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/upperair/refreshMetadata
-echo "Refreshing metadata for vil app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/vil/refreshMetadata"
+echo -e "\nRefreshing metadata for vil app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/vil/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/vil/refreshMetadata
-echo "Refreshing metadata for visibility app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/visibility/refreshMetadata"
+echo -e "\nRefreshing metadata for visibility app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/visibility/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/visibility/refreshMetadata
-echo "Refreshing metadata for visibility15 app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/visibility15/refreshMetadata"
+echo -e "\nRefreshing metadata for visibility15 app at https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/visibility15/refreshMetadata"
 curl https://mats-docker.gsd.esrl.noaa.gov/gsd/mats/visibility15/refreshMetadata
