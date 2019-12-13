b04da652edd258e827d7792258bc3023bb026682
randy.pierce <randy.pierce@noaa.gov>
2019-12-13 15:02:31 -0700

    add proxyPrefix stuff

--- mats-home/imports/ui/pages/not-found/not-found.js
+++ mats-home/imports/ui/pages/not-found/not-found.js
@@ -1,1 +1,11 @@
 import './not-found.html';
+
+Template.App_notFound.helpers({
+    proxyPrefix() {
+        if (process.env.PROXY_PREFIX) {
+            return process.env.PROXY_PREFIX + "/";
+        } else {
+            return "";
+        }
+    }
+});
