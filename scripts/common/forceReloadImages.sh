95d0d4874032dce5646c869ae2857ea67d000bd4
randy.pierce <randy.pierce@noaa.gov>
2019-12-19 17:16:01 -0700

    extract deployment environment to a parameter for forceReloadImages script

--- scripts/common/forceReloadImages.sh
+++ scripts/common/forceReloadImages.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-environment = $1
+env=$1
 
-environments=(matsdev matsint matspreint matsprod)
-function isValidEnvironment() {
-    [[ $environment =~ (^|[[:space:]])$1($|[[:space:]]) ]] && exit(0) || exit(1)
-}
-
-if [[ isValidEnvironment $environment  -ne 0 ]]; then
-  echo Usage $0 matsdev|matsint|matspreint|matsprod - you did not specify a valid environment - exiting
-  exit $1
+if [[ "$env" =~ ^(matsdev|matsint|matspreint|matsprod)$ ]]; then
+  echo reloading environment $env
+else
+  echo "Usage $0 matsdev|matsint|matspreint|matsprod  - you dod not specify a valid environment - exiting"
+  exit 1
 fi
-
 #these keys are for randy's account to rancher
 export CATTLE_ACCESS_KEY=token-7jsvp
 export CATTLE_SECRET_KEY=6nvh77zpmcg6s4z6m7l2hl82swzftzpkvf8f9xw9kbbwwkhpq9gs4v
@@ -21,16 +17,21 @@ rancher login https://rancher.gsd.esrl.noaa.gov/ --token ${TOKEN} --context c-qh
 rancher ps | grep -v NAME | grep -v mongo | grep -v mats-home | grep -v http | awk '{print $2}' | while read app
 do
 	echo "redeploying ${app}"
-	rancher kubectl --namespace=${environment} rollout restart deployment ${app}
+	rancher kubectl --namespace=${env} rollout restart deployment ${app}
 	sleep 5
 done
 sleep 10
 #force reload of any stuck pods
-stuckPods=($(rancher kubectl --namespace=${environment} get pods | grep ImageInspectError | awk '{print $1}'))
+stuckPods=($(rancher kubectl --namespace=${env} get pods | grep -v "mongo" | grep -v "mats-home" | grep http | grep ImageInspectError | awk '{print $1}'))
+containerCreating=($(rancher kubectl --namespace=${env} get pods | grep -i ContainerCreating | awk '{print $1}'))
 if [[ ${#stuckPods[@]} -gt 0 ]]; then
-	for sleepCount in 1 2 3 4 5
+	i="0"
+	while [ $i -lt 5 -o ${#containerCreating[@]} -gt 0 -o ${#stuckPods[@]} -gt 0 ]
 	do
-		rancher kubectl --namespace=${environment} delete pods $(rancher kubectl --namespace=${environment} get pods | grep ImageInspectError | awk '{print $1}')
+		rancher kubectl --namespace=${env} delete pods $(rancher kubectl --namespace=${env} get pods | grep ImageInspectError | awk '{print $1}')
+		containerCreating=($(rancher kubectl --namespace=${env} get pods | grep -v "mongo" | grep -v "mats-home" grep -v http | grep -i ContainerCreating | awk '{print $1}'))
+		stuckPods=($(rancher kubectl --namespace=${env} get pods | grep -v "mongo" | grep -v "mats-home" | grep http | grep ImageInspectError | awk '{print $1}'))
 		sleep 30
+		i=$[$i+1]
 	done
 fi
