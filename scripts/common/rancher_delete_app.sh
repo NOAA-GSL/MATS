55b314bd5fd82a264747405d6a90423da8492b97
randy.pierce <randy.pierce@noaa.gov>
2020-02-02 10:59:45 -0700

    added some rancher utilities

---
+++ scripts/common/rancher_delete_app.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# source the matsapps credentials file
+if [ ! -f ~/.matsapps_credentials ]; then
+    echo "~/.matsapps_credentials file not found!"
+    echo "you must creqate a ~/.matsapps_credentials file with the following entries.."
+    echo "# rancher cli user access for *your id*"
+    echo "export CATTLE_ENDPOINT=endpoint_from_rancher"
+    echo "export CATTLE_ACCESS_KEY=key_from_rancher"
+    echo "export CATTLE_SECRET_KEY=secret_key_from_rancher"
+    echo "export TOKEN=token_from_rancher"
+    echo "export userId=the_userId_from_the_YAML_from_your_rancher_user_access_key_token"
+    echo "Log into the rancher GUI, hover over your user icon (top right), and choose 'API and KEYS' to create your keys"
+    exit 1
+fi
+. ~/.matsapps_credentials
+
+if [[ $# -eq 0 ]]; then
+    rancher app delete $1
+else
+    rancher  app ls -q | awk -F ':' '{print $2}' | grep -v home | grep -v mongo |  while read a
+    do
+        rancher app delete $a
+    done
+fi
---
+++ scripts/common/rancher_deploy_mongo.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# source the matsapps credentials file
+if [ ! -f ~/.matsapps_credentials ]; then
+    echo "~/.matsapps_credentials file not found!"
+    echo "you must creqate a ~/.matsapps_credentials file with the following entries.."
+    echo "# rancher cli user access for *your id*"
+    echo "export CATTLE_ENDPOINT=endpoint_from_rancher"
+    echo "export CATTLE_ACCESS_KEY=key_from_rancher"
+    echo "export CATTLE_SECRET_KEY=secret_key_from_rancher"
+    echo "export TOKEN=token_from_rancher"
+    echo "export userId=the_userId_from_the_YAML_from_your_rancher_user_access_key_token"
+    echo "Log into the rancher GUI, hover over your user icon (top right), and choose 'API and KEYS' to create your keys"
+    exit 1
+fi
+. ~/.matsapps_credentials
+
+ns="matsdev"
+rootUrl=https://rancher.localhost
+pvc=matsdata
+rancher app install matsmongo mongo  -n $ns --set userId=${userId} --set defaultImage=false --set image.appVersion=${imageVersion} --set persistentVolumeClaim=${pvc} --set rootUrl=${rootUrl}
