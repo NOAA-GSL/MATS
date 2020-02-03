41d780209f33d27d55d07f7273ff083816d0b6ad
randy.pierce <randy.pierce@noaa.gov>
2020-02-03 16:05:50 -0700

    trying some quotes around METEOR_SETTINGS var in run_app

--- scripts/common/docker_scripts/run_app.sh
+++ scripts/common/docker_scripts/run_app.sh
@@ -4,11 +4,10 @@ set -e
 echo "Running app ${APPNAME} date: $(date)"
 # Set a delay to wait to start meteor container
 
-#if [[ $DELAY ]]; then
-DELAY=10
+if [[ $DELAY ]]; then
     echo "run_app => Delaying startup for $DELAY seconds"
     sleep $DELAY
-#fi
+fi
 
 # Honour already existing PORT setup
 export PORT=${PORT:-80}
@@ -27,14 +26,14 @@ EOF
   # make sure the settings sirectory and file are still modifiable.
   chmod -R 777 /usr/app/settings/${APPNAME}
 fi
-export METEOR_SETTINGS_DIR=/usr/app/settings
+export METEOR_SETTINGS_DIR="/usr/app/settings"
 echo "run_app settings are ..."
 cat /usr/app/settings/${APPNAME}/settings.json
-export METEOR_SETTINGS=$(cat /usr/app/settings/${APPNAME}/settings.json)
-echo "METEOR_SETTINGS VAR IS" ${METEOR_SETTINGS}
+export METEOR_SETTINGS="$(cat /usr/app/settings/${APPNAME}/settings.json)"
+echo "METEOR_SETTINGS VAR IS" "${METEOR_SETTINGS}"
 cd /usr/app
 if [[ $DEBUG ]]; then
-    echo "run_app => Starting meteor app for DEBUG on port: " $PORT " with settings " $METEOR_SETTINGS
+    echo "run_app => Starting meteor app for DEBUG on port: " $PORT " with settings " ${METEOR_SETTINGS}
     node --inspect=0.0.0.0:9229 main.js
 else
     echo "run_app => Starting meteor app  on port: " ${PORT} " with settings " ${METEOR_SETTINGS}
