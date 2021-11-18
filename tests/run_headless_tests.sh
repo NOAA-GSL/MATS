1a581af170299d9f535b6fdf79254413737b90df
Molly Smith <molly.b.smith@noaa.gov>
2021-11-18 13:18:48 -0700

    Modified headless tests

--- tests/run_headless_tests.sh
+++ tests/run_headless_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # example invocation...
-#./run_headless_tests.sh --maxinstances 5 --spec ~/WebstormProjects/mats-testing/src/features/ceiling15
+#./run_headless_tests.sh --maxinstances 5 --spec ~/WebstormProjects/MATS/tests/src/features/ceiling15
 if [ ! -f wdio.conf.js ]; then
     echo "No wdio.conf.js file found - not in a testing directory?"
     exit 1
@@ -9,4 +9,4 @@ rm -rf logs/*
 clear
 export HEADLESS="true"
 export NODE_OPTIONS=--max-old-space-size=16384
-meteor npx wdio run wdio.conf.js "$@" 2> >(grep -v 'TIS/TSM')
+npx wdio run wdio.conf.js "$@" 2> >(grep -v 'TIS/TSM')
