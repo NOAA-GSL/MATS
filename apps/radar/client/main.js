8b255e48c148946a46e8b910e8fc8b0432003e89
Molly Smith <molly.b.smith@noaa.gov>
2022-02-10 15:02:26 -0700

    Initial radar app; main.js works

---
+++ apps/radar/client/main.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2021 Colorado State University and Regents of the University of Colorado. All rights reserved.
+ */
+
+import {matsTypes} from 'meteor/randyp:mats-common';
+import {matsCollections} from 'meteor/randyp:mats-common';
+import {methods} from 'meteor/randyp:mats-common';
+
