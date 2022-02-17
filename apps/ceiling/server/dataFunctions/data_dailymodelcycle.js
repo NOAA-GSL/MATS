7a5d4d91c33c11948c5308e2c1bfd262ba955d92
Molly Smith <molly.b.smith@noaa.gov>
2022-02-17 15:59:30 -0700

    Combined ceiling/visibility dailymodelcycle plot type works except for 15 minutes.

--- apps/ceiling/server/dataFunctions/data_dailymodelcycle.js
+++ apps/ceiling/server/dataFunctions/data_dailymodelcycle.js
@@ -10,6 +10,7 @@ import {matsDataDiffUtils} from 'meteor/randyp:mats-common';
 import {matsDataCurveOpsUtils} from 'meteor/randyp:mats-common';
 import {matsDataProcessUtils} from 'meteor/randyp:mats-common';
 import {moment} from 'meteor/momentjs:moment';
+import toConsumableArray from "@babel/runtime/helpers/esm/toConsumableArray";
 
 dataDailyModelCycle = function (plotParams, plotFunction) {
     // initialize variables common to all curves
@@ -45,17 +46,29 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
         var curve = curves[curveIndex];
         var diffFrom = curve.diffFrom;
         var label = curve['label'];
-        var model = matsCollections['data-source'].findOne({name: 'data-source'}).optionsMap[curve['data-source']][0];
+        var database = curve['database'];
+        var databaseRef = matsCollections['database'].findOne({name: 'database'}).optionsMap[database];
+        var model = matsCollections['data-source'].findOne({name: 'data-source'}).optionsMap[database][curve['data-source']][0];
         var queryTableClause = "";
+        var truthClause = "";
+        if (database === "15 Minute Visibility") {
+            var truthStr = curve['truth'];
+            var truth = Object.keys(matsCollections['truth'].findOne({name: 'truth'}).valuesMap[database]).find(key => matsCollections['truth'].findOne({name: 'truth'}).valuesMap[database][key] === truthStr);
+        }
         var thresholdStr = curve['threshold'];
-        var threshold = Object.keys(matsCollections['threshold'].findOne({name: 'threshold'}).valuesMap).find(key => matsCollections['threshold'].findOne({name: 'threshold'}).valuesMap[key] === thresholdStr);
+        var threshold = Object.keys(matsCollections['threshold'].findOne({name: 'threshold'}).valuesMap[database]).find(key => matsCollections['threshold'].findOne({name: 'threshold'}).valuesMap[database][key] === thresholdStr);
         var thresholdClause = "";
         if (curve['utc-cycle-start'].length !== 1) {
             throw new Error("INFO:  Please select exactly one UTC Cycle Init Hour for this plot type.");
         }
         var utcCycleStart = Number(curve['utc-cycle-start'][0]);
         utcCycleStarts[curveIndex] = utcCycleStart;
-        var utcCycleStartClause = "and floor(((m0.time+1800) - m0.fcst_len*3600)%(24*3600)/3600) IN(" + utcCycleStart + ")";
+        var utcCycleStartClause;
+        if (database.includes("15 Minute")) {
+            utcCycleStartClause = "and floor(((m0.time+450) - (m0.fcst_len*60+m0.fcst_min)*60)%(24*3600)/900)/4 IN(" + utcCycleStart + ")";
+        } else {
+            utcCycleStartClause = "and floor(((m0.time+1800) - m0.fcst_len*3600)%(24*3600)/3600) IN(" + utcCycleStart + ")";
+        }
         var forecastLengthClause = "and m0.fcst_len < 24";
         var dateClause;
         var siteDateClause = "";
@@ -65,25 +78,38 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
         var statisticSelect = curve['statistic'];
         var statisticOptionsMap = matsCollections['statistic'].findOne({name: 'statistic'}, {optionsMap: 1})['optionsMap'];
         var statisticClause;
-        var queryPool;
         var regionType = curve['region-type'];
         if (regionType === 'Predefined region') {
             var regionStr = curve['region'];
             var region = Object.keys(matsCollections['region'].findOne({name: 'region'}).valuesMap).find(key => matsCollections['region'].findOne({name: 'region'}).valuesMap[key] === regionStr);
-            queryTableClause = "from " + model + "_" + region + " as m0";
+            queryTableClause = "from " + databaseRef.sumsDB + "." + model + "_" + region + " as m0";
+            if (database === "15 Minute Visibility") {
+                truthClause = "and m0.truth = '" + truth + "'";
+            }
             thresholdClause = "and m0.trsh = " + threshold;
-            statisticClause = "sum(m0.yy) as hit, sum(m0.yn) as fa, sum(m0.ny) as miss, sum(m0.nn) as cn, group_concat(m0.time, ';', m0.yy, ';', m0.yn, ';', m0.ny, ';', m0.nn order by m0.time) as sub_data, count(m0.yy) as N0";;
+            statisticClause = "sum(m0.yy) as hit, sum(m0.yn) as fa, sum(m0.ny) as miss, sum(m0.nn) as cn, group_concat(m0.time, ';', m0.yy, ';', m0.yn, ';', m0.ny, ';', m0.nn order by m0.time) as sub_data, count(m0.yy) as N0";
             dateClause = "and m0.time >= " + fromSecs + " and m0.time <= " + toSecs;
-            queryPool = sumPool;
         } else {
             var obsTable = (model.includes('ret_') || model.includes('Ret_')) ? 'obs_retro' : 'obs';
-            queryTableClause = "from " + obsTable + " as o, " + model + " as m0 ";
+            queryTableClause = "from " + databaseRef.modelDB + "." + obsTable + " as o, " + databaseRef.modelDB + "." + model + " as m0 ";
             statisticClause = "sum(if((m0.ceil < {{threshold}}) and (o.ceil < {{threshold}}),1,0)) as hit, sum(if((m0.ceil < {{threshold}}) and NOT (o.ceil < {{threshold}}),1,0)) as fa, " +
                 "sum(if(NOT (m0.ceil < {{threshold}}) and (o.ceil < {{threshold}}),1,0)) as miss, sum(if(NOT (m0.ceil < {{threshold}}) and NOT (o.ceil < {{threshold}}),1,0)) as cn, " +
                 "group_concat(ceil(3600*floor((m0.time+1800)/3600)), ';', if((m0.ceil < {{threshold}}) and (o.ceil < {{threshold}}),1,0), ';', " +
                 "if((m0.ceil < {{threshold}}) and NOT (o.ceil < {{threshold}}),1,0), ';', if(NOT (m0.ceil < {{threshold}}) and (o.ceil < {{threshold}}),1,0), ';', " +
                 "if(NOT (m0.ceil < {{threshold}}) and NOT (o.ceil < {{threshold}}),1,0) order by ceil(3600*floor((m0.time+1800)/3600))) as sub_data, count(m0.ceil) as N0";
             statisticClause = statisticClause.replace(/\{\{threshold\}\}/g, threshold);
+            if (database.includes("Visibility")) {
+                statisticClause = statisticClause.replace(/m0\.ceil/g, "m0.vis100");
+                statisticClause = statisticClause.replace(/o\.ceil/g, "o.vis100");
+                if (database === "15 Minute Visibility") {
+                    if (truth !== "qc") {
+                        statisticClause = statisticClause.replace(/o\.vis100/g, "o.vis_" + truth);
+                    } else {
+                        statisticClause = statisticClause.replace(/o\.vis100/g, "o.vis_closest");
+                        truthClause = "and o.vis_std < 2.4";
+                    }
+                }
+            }
             var sitesList = curve['sites'] === undefined ? [] : curve['sites'];
             var querySites = [];
             if (sitesList.length > 0 && sitesList !== matsTypes.InputTypes.unused) {
@@ -103,7 +129,6 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
             dateClause = "and m0.time >= " + fromSecs + " - 900 and m0.time <= " + toSecs + " + 900";
             siteDateClause = "and o.time >= " + fromSecs + " - 900 and o.time <= " + toSecs + " + 900";
             siteMatchClause = "and m0.madis_id = o.madis_id and m0.time = o.time ";
-            queryPool = modelPool;
         }
         // axisKey is used to determine which axis a curve should use.
         // This axisKeySet object is used like a set and if a curve has the same
@@ -135,6 +160,7 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
                 "{{utcCycleStartClause}} " +
                 "{{thresholdClause}} " +
                 "{{forecastLengthClause}} " +
+                "{{truthClause}} " +
                 "group by avtime " +
                 "order by avtime" +
                 ";";
@@ -143,11 +169,15 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
             statement = statement.replace('{{queryTableClause}}', queryTableClause);
             statement = statement.replace('{{siteMatchClause}}', siteMatchClause);
             statement = statement.replace('{{sitesClause}}', sitesClause);
-            statement = statement.replace('{{thresholdClause}}', thresholdClause);
             statement = statement.replace('{{utcCycleStartClause}}', utcCycleStartClause);
+            statement = statement.replace('{{thresholdClause}}', thresholdClause);
             statement = statement.replace('{{forecastLengthClause}}', forecastLengthClause);
+            statement = statement.replace('{{truthClause}}', truthClause);
             statement = statement.replace('{{dateClause}}', dateClause);
             statement = statement.replace('{{siteDateClause}}', siteDateClause);
+            if (database === "15 Minute Visibility") {
+                statement = statement.replace(/o\.time/g, "o.valid_time");
+            }
             dataRequests[label] = statement;
 
             var queryResult;
@@ -155,7 +185,7 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
             var finishMoment;
             try {
                 // send the query statement to the query function
-                queryResult = matsDataQueryUtils.queryDBSpecialtyCurve(queryPool, statement, appParams, statisticSelect);
+                queryResult = matsDataQueryUtils.queryDBSpecialtyCurve(sumPool, statement, appParams, statisticSelect);
                 finishMoment = moment();
                 dataRequests["data retrieval (query) time - " + label] = {
                     begin: startMoment.format(),
