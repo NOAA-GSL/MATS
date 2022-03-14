ee8277a7b7430dc82a5c68198883af7be3d3d482
Molly Smith <molly.b.smith@noaa.gov>
2022-03-14 13:51:38 -0600

    Bug fix associated with Steve's request to have the UTC selector be multi-select

--- apps/ceil-vis/server/main.js
+++ apps/ceil-vis/server/main.js
@@ -727,7 +727,7 @@ const doCurveParams = function () {
                 selected: '',
                 controlButtonCovered: true,
                 unique: false,
-                default: 12,
+                default: ['12'],
                 controlButtonVisibility: 'block',
                 controlButtonText: "utc cycle init hour",
                 displayOrder: 4,
