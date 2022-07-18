ff30a83dc3b60016f00952ea296c965831a08eff
randy.pierce <randy.pierce@noaa.gov>
2022-07-18 17:13:39 -0600

    modified test cases for station queries

--- apps/cb-ceiling/test/time_series_station_query.test.js
+++ apps/cb-ceiling/test/time_series_station_query.test.js
@@ -14,40 +14,53 @@ const { Console } = require('console');
 
     it('should return data', function (done) {
       try {
-        const statement = `SELECT m0.fcstValidEpoch fve,
-        COUNT(DISTINCT m0.fcstValidEpoch) N_times,
-        COUNT(odata.name) AS N0,
-        ARRAY_AGG(m0data.Ceiling) as mSubValues,
-        ARRAY_AGG(odata.Ceiling) as oSubValues,
-        SUM (CASE WHEN m0data.Ceiling < 500 AND odata.Ceiling < 500 THEN 1 ELSE 0 END) AS hit,
-        SUM (CASE WHEN m0data.Ceiling < 500 AND NOT odata.Ceiling < 500 THEN 1 ELSE 0 END) AS false_alarm,
-        SUM (CASE WHEN NOT m0data.Ceiling < 500 AND odata.Ceiling < 500 THEN 1 ELSE 0 END) AS miss,
-        SUM (CASE WHEN NOT m0data.Ceiling < 500 AND NOT odata.Ceiling < 500 THEN 1 ELSE 0 END) AS correct_negative
-        FROM mdata AS m0 USE INDEX (ix_subset_version_model_fcstLen_fcstValidEpoc)
+        const statement = `
+        SELECT m0.fcstValidEpoch fve,
+        COUNT(DISTINCT m0.fcstValidEpoch) AS N_times,
+        SUM(CASE WHEN m0data.Ceiling < 500
+                AND odata.Ceiling < 500 THEN 1 ELSE 0 END) +
+        SUM(CASE WHEN m0data.Ceiling < 500
+                AND NOT odata.Ceiling < 500 THEN 1 ELSE 0 END) +
+        SUM(CASE WHEN NOT m0data.Ceiling < 500
+                AND odata.Ceiling < 500 THEN 1 ELSE 0 END) +
+        SUM(CASE WHEN NOT m0data.Ceiling < 500
+                AND NOT odata.Ceiling < 500 THEN 1 ELSE 0 END) AS N0,
+        ARRAY_AGG(TO_STRING(m0.fcstValidEpoch) || ";" || CASE WHEN m0data.Ceiling < 500
+                AND odata.Ceiling < 500 THEN "1" ELSE "0" END || ";" || CASE WHEN m0data.Ceiling < 500
+                AND NOT odata.Ceiling < 500 THEN "1" ELSE "0" END || ";" || CASE WHEN NOT m0data.Ceiling < 500
+                AND odata.Ceiling < 500 THEN "1" ELSE "0" END || ";" || CASE WHEN NOT m0data.Ceiling < 500
+                AND NOT odata.Ceiling < 500 THEN "1" ELSE "0" END) AS subData,
+        SUM(CASE WHEN m0data.Ceiling < 500
+                AND odata.Ceiling < 500 THEN 1 ELSE 0 END) AS hit,
+        SUM(CASE WHEN m0data.Ceiling < 500
+                AND NOT odata.Ceiling < 500 THEN 1 ELSE 0 END) AS false_alarm,
+        SUM(CASE WHEN NOT m0data.Ceiling < 500
+                AND odata.Ceiling < 500 THEN 1 ELSE 0 END) AS miss,
+        SUM(CASE WHEN NOT m0data.Ceiling < 500
+                AND NOT odata.Ceiling < 500 THEN 1 ELSE 0 END) AS correct_negative
+          FROM mdata AS m0 USE INDEX (ix_subset_version_model_fcstLen_fcstValidEpoc)
             JOIN mdata AS o USE INDEX(adv_fcstValidEpoch_docType_subset_version_type) ON o.fcstValidEpoch = m0.fcstValidEpoch
-        UNNEST o.data AS odata
-        UNNEST m0.data AS m0data
-            JOIN m0data AS md ON odata.name = m0data.name
-        WHERE o.type='DD'
-            AND o.docType='obs'
-            AND o.subset='METAR'
-            AND o.version='V01'
-            AND m0.type='DD'
-            AND m0.docType='model'
-            AND m0.subset='METAR'
-            AND m0.version='V01'
-            AND m0.model='RAP_OPS_130'
-            AND m0data.name IN ['KGKY','KY19','KY49','KY51']
-            AND odata.name IN ['KGKY','KY19','KY49','KY51']
+          UNNEST o.data AS odata
+          UNNEST m0.data AS m0data
+          WHERE o.type="DD"
+            AND o.docType="obs"
+            AND o.subset="METAR"
+            AND o.version="V01"
+            AND m0.type="DD"
+            AND m0.docType="model"
+            AND m0.subset="METAR"
+            AND m0.version="V01"
+            AND m0.model="RAP_OPS_130"
+            AND m0data.name IN ["KGKY","KY19","KY49","KY51"]
+            AND odata.name IN ["KGKY","KY19","KY49","KY51"]
             AND o.fcstValidEpoch >= 1655242200
-            AND o.fcstValidEpoch <= 1655242200 + 24 * 4 * 36000
+            AND o.fcstValidEpoch <= 1655242200 + 24 * 3600 * 30
             AND m0.fcstValidEpoch >= 1655242200
-            AND m0.fcstValidEpoch <= 1655242200 + 24 * 4 * 3600
+            AND m0.fcstValidEpoch <= 1655242200 + 24 * 3600 * 30
             AND m0.fcstValidEpoch = o.fcstValidEpoch
-            AND m0.fcstLen IN [1,3,6,9,12,15,18,21]
             AND m0.fcstLen = 12
-        GROUP BY m0.fcstValidEpoch
-        ORDER BY m0.fcstValidEpoch;`
+          GROUP BY m0.fcstValidEpoch
+          ORDER BY m0.fcstValidEpoch;`
           console.log(statement);
         data = "something"
         chai.assert(data == "something");
