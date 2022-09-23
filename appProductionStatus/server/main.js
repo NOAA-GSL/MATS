164a81a6a1ba4a1732de8de541449b4517a5bc87
Molly Smith <molly.b.smith@noaa.gov>
2022-09-23 11:25:02 -0600

    Removed appProductionStatus code, as we have moved to github-based build and deploy workflows.

--- appProductionStatus/server/main.js
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright (c) 2019. Colorado State University and Regents of the University of Colorado. All rights reserved.
- */
-
-import '../imports/api/buildConfiguration.js';
-import '../imports/api/deployments.js';
