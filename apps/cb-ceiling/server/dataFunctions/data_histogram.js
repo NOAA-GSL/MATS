aea1eb13a3da1ded7abd11dfd3d396eb3901ac30
gopa-noaa <gopa.padmanabhan@.noaa.gov>
2023-03-07 00:01:41 +0000

    Histogram plots are working

--- apps/cb-ceiling/server/dataFunctions/data_histogram.js
+++ apps/cb-ceiling/server/dataFunctions/data_histogram.js
@@ -2,16 +2,19 @@
  * Copyright (c) 2021 Colorado State University and Regents of the University of Colorado. All rights reserved.
  */
 
-import {
+import
+{
     matsCollections,
     matsTypes,
     matsDataUtils,
     matsDataQueryUtils,
     matsDataProcessUtils
 } from 'meteor/randyp:mats-common';
-import {moment} from 'meteor/momentjs:moment';
+import { moment } from 'meteor/momentjs:moment';
 
-dataHistogram = function (plotParams, plotFunction) {
+dataHistogram = function (plotParams, plotFunction)
+{
+    var fs = require("fs");
     // initialize variables common to all curves
     const appParams = {
         "plotType": matsTypes.PlotTypes.histogram,
@@ -39,49 +42,63 @@ dataHistogram = function (plotParams, plotFunction) {
     const yAxisFormat = binParams.yAxisFormat;
     const binNum = binParams.binNum;
 
-    for (var curveIndex = 0; curveIndex < curvesLength; curveIndex++) {
+    for (var curveIndex = 0; curveIndex < curvesLength; curveIndex++)
+    {
         // initialize variables specific to each curve
         var curve = curves[curveIndex];
+
+        queryTemplate = fs.readFileSync(
+            process.env.PWD +
+            "/server/dataFunctions/sqlTemplates/tmpl_Histogram.sql",
+            "utf8"
+        );
+        console.log("\nqueryTemplate:\n" + queryTemplate);
+
         var diffFrom = curve.diffFrom;
         dataFoundForCurve[curveIndex] = true;
         var label = curve['label'];
         var variable = curve['variable'];
-        var model = matsCollections['data-source'].findOne({name: 'data-source'}).optionsMap[variable][curve['data-source']][0];
-        var modelClause = "AND m0.model='" + model + "' ";
-        var queryTableClause = "from vxDBTARGET  m0";
+        var model = matsCollections['data-source'].findOne({ name: 'data-source' }).optionsMap[variable][curve['data-source']][0];
         var thresholdStr = curve['threshold'];
-        var threshold = Object.keys(matsCollections['threshold'].findOne({name: 'threshold'}).valuesMap[variable]).find(key => matsCollections['threshold'].findOne({name: 'threshold'}).valuesMap[variable][key] === thresholdStr);
+        var threshold = Object.keys(matsCollections['threshold'].findOne({ name: 'threshold' }).valuesMap[variable]).find(key => matsCollections['threshold'].findOne({ name: 'threshold' }).valuesMap[variable][key] === thresholdStr);
         threshold = threshold.replace(/_/g, ".");
-        var validTimeClause = "";
         var validTimes = curve['valid-time'] === undefined ? [] : curve['valid-time'];
-        if (validTimes.length !== 0 && validTimes !== matsTypes.InputTypes.unused) {
-            validTimeClause = "and m0.fcstValidEpoch%(24*3600)/3600 IN[" + validTimes + "]";
+        if (validTimes.length !== 0 && validTimes !== matsTypes.InputTypes.unused)
+        {
+            queryTemplate = queryTemplate.replace(
+                /vxVALID_TIMES/g,
+                cbPool.trfmListToCSVString(validTimes, null, false)
+            );
+        } else
+        {
+            queryTemplate = cbPool.trfmSQLRemoveClause(
+                queryTemplate,
+                "vxVALID_TIMES"
+            );
         }
+
         var forecastLength = curve['forecast-length'];
         var forecastLengthClause = "and m0.fcstLen = " + forecastLength;
         var dateRange = matsDataUtils.getDateRange(curve['curve-dates']);
         var fromSecs = dateRange.fromSeconds;
         var toSecs = dateRange.toSeconds;
         var statisticSelect = curve['statistic'];
-        var statisticOptionsMap = matsCollections['statistic'].findOne({name: 'statistic'}, {optionsMap: 1})['optionsMap'];
+        var statisticOptionsMap = matsCollections['statistic'].findOne({ name: 'statistic' }, { optionsMap: 1 })['optionsMap'];
         var regionType = curve['region-type'];
-        if (regionType === 'Select stations') {
+        if (regionType === 'Select stations')
+        {
             throw new Error("INFO:  Single/multi station plotting is not available for histograms.");
         }
         var regionStr = curve['region'];
-        var region = Object.keys(matsCollections['region'].findOne({name: 'region'}).valuesMap).find(key => matsCollections['region'].findOne({name: 'region'}).valuesMap[key] === regionStr);
-        var regionClause = "AND m0.region='" + region + "' ";
-        var statisticClause = "sum(m0.data.['" + threshold + "'].hits) hit, sum(m0.data.['" + threshold + "'].false_alarms) fa, " +
-            "sum(m0.data.['" + threshold + "'].misses) miss, sum(m0.data.['" + threshold + "'].correct_negatives) cn, " +
-            "ARRAY_SORT(ARRAY_AGG(TO_STRING(m0.fcstValidEpoch) || ';' || TO_STRING(m0.data.['" + threshold + "'].hits) || ';' || " +
-            "TO_STRING(m0.data.['" + threshold + "'].false_alarms) || ';' || TO_STRING(m0.data.['" + threshold + "'].misses) || ';' || " +
-            "TO_STRING(m0.data.['" + threshold + "'].correct_negatives))) sub_data, count(m0.data.['" + threshold + "'].hits) N0 ";
-        var dateClause = "and m0.fcstValidEpoch >= " + fromSecs + " and m0.fcstValidEpoch <= " + toSecs;
-        var whereClause = "WHERE " +
-            "m0.type='DD' " +
-            "AND m0.docType='CTC' " +
-            "AND m0.subset='METAR' " +
-            "AND m0.version='V01' ";
+        var region = Object.keys(matsCollections['region'].findOne({ name: 'region' }).valuesMap).find(key => matsCollections['region'].findOne({ name: 'region' }).valuesMap[key] === regionStr);
+
+        queryTemplate = queryTemplate.replace(/vxFROM_SECS/g, fromSecs);
+        queryTemplate = queryTemplate.replace(/vxTO_SECS/g, toSecs);
+        queryTemplate = queryTemplate.replace(/vxTHRESHOLD/g, threshold);
+        queryTemplate = queryTemplate.replace(/vxFCST_LEN/g, forecastLength);
+        queryTemplate = queryTemplate.replace(/vxREGION/g, region);
+        queryTemplate = queryTemplate.replace(/vxMODEL/g, model);
+
         // axisKey is used to determine which axis a curve should use.
         // This axisKeySet object is used like a set and if a curve has the same
         // units (axisKey) it will use the same axis.
@@ -89,48 +106,26 @@ dataHistogram = function (plotParams, plotFunction) {
         var statType = statisticOptionsMap[statisticSelect][0];
         var varUnits = statisticOptionsMap[statisticSelect][1];
         var axisKey = yAxisFormat;
-        if (yAxisFormat === 'Relative frequency') {
+        if (yAxisFormat === 'Relative frequency')
+        {
             axisKey = axisKey + " (x100)"
         }
         curves[curveIndex].axisKey = axisKey; // stash the axisKey to use it later for axis options
         curves[curveIndex].binNum = binNum; // stash the binNum to use it later for bar chart options
 
         var d;
-        if (diffFrom == null) {
+        if (diffFrom == null)
+        {
             // this is a database driven curve, not a difference curve
             // prepare the query from the above parameters
-            var statement = "SELECT m0.fcstValidEpoch AS avtime, " +
-                "COUNT(DISTINCT m0.fcstValidEpoch) N_times, " +
-                "MIN(m0.fcstValidEpoch) min_secs, " +
-                "MAX(m0.fcstValidEpoch) max_secs, " +
-                "{{statisticClause}} " +
-                "{{queryTableClause}} " +
-                "{{whereClause}} " +
-                "{{modelClause}} " +
-                "{{regionClause}} " +
-                "{{dateClause}} " +
-                "{{validTimeClause}} " +
-                "{{forecastLengthClause}} " +
-                "GROUP BY m0.fcstValidEpoch " +
-                "ORDER BY avtime" +
-                ";";
-
-            statement = statement.replace('{{statisticClause}}', statisticClause);
-            statement = statement.replace('{{queryTableClause}}', queryTableClause);
-            statement = statement.replace('{{whereClause}}', whereClause);
-            statement = statement.replace('{{modelClause}}', modelClause);
-            statement = statement.replace('{{regionClause}}', regionClause);
-            statement = statement.replace('{{validTimeClause}}', validTimeClause);
-            statement = statement.replace('{{forecastLengthClause}}', forecastLengthClause);
-            statement = statement.replace('{{dateClause}}', dateClause);
-
-            statement = cbPool.trfmSQLForDbTarget(statement);
+            statement = cbPool.trfmSQLForDbTarget(queryTemplate);
             dataRequests[label] = statement;
 
             var queryResult;
             var startMoment = moment();
             var finishMoment;
-            try {
+            try
+            {
                 // send the query statement to the query function
                 queryResult = matsDataQueryUtils.queryDBSpecialtyCurve(cbPool, statement, appParams, statisticSelect);
                 finishMoment = moment();
@@ -144,27 +139,33 @@ dataHistogram = function (plotParams, plotFunction) {
                 d = queryResult.data;
                 allReturnedSubStats.push(d.subVals); // save returned data so that we can calculate histogram stats once all the queries are done
                 allReturnedSubSecs.push(d.subSecs);
-            } catch (e) {
+            } catch (e)
+            {
                 // this is an error produced by a bug in the query function, not an error returned by the mysql database
                 e.message = "Error in queryDB: " + e.message + " for statement: " + statement;
                 throw new Error(e.message);
             }
-            if (queryResult.error !== undefined && queryResult.error !== "") {
-                if (queryResult.error === matsTypes.Messages.NO_DATA_FOUND) {
+            if (queryResult.error !== undefined && queryResult.error !== "")
+            {
+                if (queryResult.error === matsTypes.Messages.NO_DATA_FOUND)
+                {
                     // this is NOT an error just a no data condition
                     dataFoundForCurve[curveIndex] = false;
-                } else {
+                } else
+                {
                     // this is an error returned by the mysql database
                     error += "Error from verification query: <br>" + queryResult.error + "<br> query: <br>" + statement + "<br>";
                     throw (new Error(error));
                 }
-            } else {
+            } else
+            {
                 dataFoundForAnyCurve = true;
             }
         }
     }
 
-    if (!dataFoundForAnyCurve) {
+    if (!dataFoundForAnyCurve)
+    {
         // we found no data for any curves so don't bother proceeding
         throw new Error("INFO:  No valid data for any curves.");
     }
---
+++ apps/cb-ceiling/server/dataFunctions/sqlTemplates/orgSQLs/Histogram.sql
@@ -0,0 +1,23 @@
+SELECT m0.fcstValidEpoch AS avtime,
+       COUNT(DISTINCT m0.fcstValidEpoch) N_times,
+       MIN(m0.fcstValidEpoch) min_secs,
+       MAX(m0.fcstValidEpoch) max_secs,
+       SUM(m0.data.['500.0'].hits) hit,
+       SUM(m0.data.['500.0'].false_alarms) fa,
+       SUM(m0.data.['500.0'].misses) miss,
+       SUM(m0.data.['500.0'].correct_negatives) cn,
+       ARRAY_SORT(ARRAY_AGG(TO_STRING(m0.fcstValidEpoch) || ';' || TO_STRING(m0.data.['500.0'].hits) || ';' || TO_STRING(m0.data.['500.0'].false_alarms) || ';' || TO_STRING(m0.data.['500.0'].misses) || ';' || TO_STRING(m0.data.['500.0'].correct_negatives))) sub_data,
+       COUNT(m0.data.['500.0'].hits) N0
+FROM vxdata._default.METAR m0
+WHERE m0.type='DD'
+    AND m0.docType='CTC'
+    AND m0.subset='METAR'
+    AND m0.version='V01'
+    AND m0.model='RAP_OPS_130'
+    AND m0.region='ALL_HRRR'
+    AND m0.fcstValidEpoch >= 1668272400
+    AND m0.fcstValidEpoch <= 1670864400
+    AND m0.fcstValidEpoch%(24*3600)/3600 IN[1,3,4,5]
+    AND m0.fcstLen = 6
+GROUP BY m0.fcstValidEpoch
+ORDER BY avtime;
