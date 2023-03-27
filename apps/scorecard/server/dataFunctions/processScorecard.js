NOAA-GSL/MATS

commit d369ce4a10b3c116644a091ec4b613fb00eb06ff

add app url to document

apps/scorecard/server/dataFunctions/processScorecard.js
@@ -95,6 +95,7 @@ processScorecard = function (plotParams, plotFunction) {
         "title":userName + " " + name + " " + submitDate + " " + processedDate + " " + daterange,
         blocks: {
             Blocklabel1: {
+                "appUrl": "the app url",
                 "blockTitle":{"label":"Block label","dataSource":"dataSource","controlDataSource":"controlDataSource"},
                 "blockParameters":{"param1:"param1 value", "param2":"param2 value",.....},
                 "regions":["region1":"region2"....],
@@ -394,6 +395,9 @@ processScorecard = function (plotParams, plotFunction) {
       dataSource: curve["data-source"],
       controlDataSource: curve["control-data-source"],
     };
+    const appName = Meteor.settings.public.app;
+    const appUrl = Meteor.settings.public.home + "/" + appName;
+    scorecardDocument.results.blocks[curve.label].blockApplication = appUrl
     scorecardDocument.results.blocks[curve.label].blockParameters = blockParameters;
     scorecardDocument.results.blocks[curve.label].regions = regions;
     scorecardDocument.results.blocks[curve.label].fcstlens = fcstLengths;

commit e9101bad520a63b3274d5a7c63e9ba41fb0f9aec

Fix a missing-semicolon bug and a linter error
These were introduced in d369ce4.

apps/scorecard/server/dataFunctions/processScorecard.js
@@ -396,8 +396,8 @@ processScorecard = function (plotParams, plotFunction) {
       controlDataSource: curve["control-data-source"],
     };
     const appName = Meteor.settings.public.app;
-    const appUrl = Meteor.settings.public.home + "/" + appName;
-    scorecardDocument.results.blocks[curve.label].blockApplication = appUrl
+    const appUrl = `${Meteor.settings.public.home}/${appName}`;
+    scorecardDocument.results.blocks[curve.label].blockApplication = appUrl;
     scorecardDocument.results.blocks[curve.label].blockParameters = blockParameters;
     scorecardDocument.results.blocks[curve.label].regions = regions;
     scorecardDocument.results.blocks[curve.label].fcstlens = fcstLengths;