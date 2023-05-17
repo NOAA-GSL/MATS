a146445063439a2a03a8a93f00823483690771e6
Molly Smith <molly.b.smith@noaa.gov>
2023-05-17 13:44:06 -0600

    Initial attempt converting precip selective metadata

--- scripts/matsMetaDataForApps/createMetaData/mysql/mats/update_metadata_precip.py
+++ scripts/matsMetaDataForApps/createMetaData/mysql/mats/update_metadata_precip.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python
+#!/scratch1/BMC/amb-verif/miniconda/miniconda3/envs/avid_verify_py3/bin/python
+#
+# The reason I am hardcoding the python path above is that this script is usally run by model developers 
+# without guidance from us, and I don't want them to be tripped up by the fact that the default puthon on 
+# Hera is python 2, while this script requires python 3. There's also an error to that effect below, but 
+#I'm trying to cut down on the number of confused emails we get. Our main scripts are all environment-agnostic, 
+#becuause they are run by verification team members who know which conda environment to use.
 #
 # Updates the regions_per_model_mats_all_categories table for all models in precip
 
@@ -8,7 +14,13 @@ from datetime import datetime
 
 import re
 import sys
-import MySQLdb
+
+try:
+    import pymysql
+except ImportError:
+    raise ImportError('--------------------IMPORTANT: This script now requires python 3 to run. \
+                      You can do this in the amb-verif conda environment by running "conda activate \
+                      avid_verify_py3" and then trying this script again.-------------------------')
 
 
 ############################################################################
@@ -21,7 +33,7 @@ def update_rpm_record(cnx, cursor, table_name, display_text, regions, fcst_types
     cursor.execute(find_rpm_rec)
     build_record_id = int(0)
     for row in cursor:
-        val = row.values()[0]
+        val = list(row.values())[0]
         build_record_id = int(val)
 
     # see if this record already exists in the prod table
@@ -30,7 +42,7 @@ def update_rpm_record(cnx, cursor, table_name, display_text, regions, fcst_types
     cursor.execute(find_rpm_rec)
     prod_record_id = int(0)
     for row in cursor:
-        val = row.values()[0]
+        val = list(row.values())[0]
         prod_record_id = int(val)
 
     if len(regions) > int(0) and len(fcst_types) > int(0):
@@ -114,26 +126,26 @@ def update_rpm_record(cnx, cursor, table_name, display_text, regions, fcst_types
 def reprocess_specific_metadata(models_to_reprocess):
     # connect to database
     try:
-        cnx = MySQLdb.connect(read_default_file="/home/role.amb-verif/.my.cnf")  # location of cnf file on Hera; edit if running locally
+        cnx = pymysql.connect(read_default_file="/home/role.amb-verif/.my.cnf")  # location of cnf file on Hera; edit if running locally
         cnx.autocommit = True
-        cursor = cnx.cursor(MySQLdb.cursors.DictCursor)
-    except MySQLdb.Error as e:
+        cursor = cnx.cursor(pymysql.cursors.DictCursor)
+    except pymysql.Error as e:
         print("Error: " + str(e))
         sys.exit(1)
 
     try:
-        cnx2 = MySQLdb.connect(read_default_file="/home/role.amb-verif/.my.cnf")
+        cnx2 = pymysql.connect(read_default_file="/home/role.amb-verif/.my.cnf")
         cnx2.autocommit = True
-        cursor2 = cnx2.cursor(MySQLdb.cursors.DictCursor)
-    except MySQLdb.Error as e:
+        cursor2 = cnx2.cursor(pymysql.cursors.DictCursor)
+    except pymysql.Error as e:
         print("Error: " + str(e))
         sys.exit(1)
 
     try:
-        cnx3 = MySQLdb.connect(read_default_file="/home/role.amb-verif/.my.cnf")
+        cnx3 = pymysql.connect(read_default_file="/home/role.amb-verif/.my.cnf")
         cnx3.autocommit = True
-        cursor3 = cnx3.cursor(MySQLdb.cursors.DictCursor)
-    except MySQLdb.Error as e:
+        cursor3 = cnx3.cursor(pymysql.cursors.DictCursor)
+    except pymysql.Error as e:
         print("Error: " + str(e))
         sys.exit(1)
 
@@ -177,19 +189,21 @@ def reprocess_specific_metadata(models_to_reprocess):
     get_model_orders = "select model,m_order from primary_model_orders order by m_order;"
     cursor3.execute(get_model_orders)
 
-    new_model_list = main_models.values()
+    new_model_list = list(main_models.values())
+    main_model_order_keys = []
     main_model_orders = {}
     for row in cursor3:
         new_model = str(row['model'])
         m_order = int(row['m_order'])
         if new_model in new_model_list:
+            main_model_order_keys.append(new_model)
             main_model_orders[new_model] = m_order
 
     # get max category used so far
     cursor3.execute(usedb)
     cursor3.execute("select max(display_category) from regions_per_model_mats_all_categories;")
     for row in cursor3:
-        max_display_category = row.values()[0]
+        max_display_category = list(row.values())[0]
     curr_model_order = 1
 
     cursor3.close()
@@ -208,7 +222,7 @@ def reprocess_specific_metadata(models_to_reprocess):
         per_model[model]['maxdate'] = 0
         per_model[model]['numrecs'] = 0
 
-        if model in main_model_keys:
+        if model in main_model_keys and main_models[model] in main_model_order_keys:
             per_model[model]['display_text'] = main_models[model]
             per_model[model]['display_category'] = 1
             per_model[model]['display_order'] = main_model_orders[per_model[model]['display_text']]
@@ -229,8 +243,7 @@ def reprocess_specific_metadata(models_to_reprocess):
         show_tables = ("show tables like '" + model + "_%';")
         cursor.execute(show_tables)
         for row in cursor:
-            tablename = row.values()[0]
-            tablename = tablename.encode('ascii', 'ignore')
+            tablename = str(list(row.values())[0])
             table_model = re.sub('_[0-9][0-9]km_.*', '', tablename)
             if table_model == model:
                 # this is a table that does belong to this model
@@ -265,7 +278,7 @@ def reprocess_specific_metadata(models_to_reprocess):
                     cursor2.execute(get_fcst_types)
                     thisfcst_types = []
                     for row2 in cursor2:
-                        val = row2.values()[0]
+                        val = list(row2.values())[0]
                         thisfcst_types.append(int(val))
                     per_model[model]['fcst_type'] = list(set(per_model[model]['fcst_type']) | set(thisfcst_types))
                     per_model[model]['fcst_type'].sort(key=int)
@@ -274,7 +287,7 @@ def reprocess_specific_metadata(models_to_reprocess):
                     cursor2.execute(get_trsh)
                     thistrsh = []
                     for row2 in cursor2:
-                        val = row2.values()[0]
+                        val = list(row2.values())[0]
                         thistrsh.append(int(val * 100))
                     per_model[model]['trshs'] = list(set(per_model[model]['trshs']) | set(thistrsh))
                     per_model[model]['trshs'].sort(key=int)
