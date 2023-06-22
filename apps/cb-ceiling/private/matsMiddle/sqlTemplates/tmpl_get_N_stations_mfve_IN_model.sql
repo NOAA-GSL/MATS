51d9044f58544766d4dcc6bb41f78005a71fdce1
gopa-noaa <gopa.padmanabhan@.noaa.gov>
2023-06-22 16:25:02 +0000

    CTC summed by epoch

--- apps/cb-ceiling/private/matsMiddle/sqlTemplates/tmpl_get_N_stations_mfve_IN_model.sql
+++ apps/cb-ceiling/private/matsMiddle/sqlTemplates/tmpl_get_N_stations_mfve_IN_model.sql
@@ -10,5 +10,6 @@ WHERE
     AND docType = "model"
     AND model = {{vxMODEL}}
     AND fcstLen = {{vxFCST_LEN}}
+    AND fcstLen IN {{vxFCST_LEN_ARRAY}}
     AND version = "V01"
     AND fcstValidEpoch IN {{fcstValidEpoch}}
