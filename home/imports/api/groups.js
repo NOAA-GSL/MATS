60b34bb76fb82850dc510d12b7a207f714947209
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2023-08-01 14:58:03 -0600

    Cleanup previous home apps

--- home/imports/api/groups.js
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright (c) 2021 Colorado State University and Regents of the University of Colorado. All rights reserved.
- */
-
-import { Mongo  } from 'meteor/mongo';
-export const Groups = new Mongo.Collection('groups');
