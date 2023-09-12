917ef7f7207007b04fa436b945269c8b832ae4a9
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2023-09-11 19:52:37 -0600

    Expand and collapse app group headers
    This commit makes a number of changes:
    - Utilizes const & let in place of var
    - Adds an "appgroup" class to the group headers
    - Adds event listeners to expand & collapse the group headers when
      their child elements are also collapsed.

--- home/static/js/home.js
+++ home/static/js/home.js
@@ -3,26 +3,67 @@
 
 // Hides all ".multi-collapse" elements, then shows the elements with the given selector
 function showSelector(selector) {
-    var collapseElements = document.querySelectorAll('.multi-collapse');
-    var showElements = document.querySelectorAll(selector);
+  const collapseElements = document.querySelectorAll(".multi-collapse");
+  const showElements = document.querySelectorAll(selector);
 
-    collapseElements.forEach(function(element) {
-      var bsCollapse = new bootstrap.Collapse(element, {toggle: false});
-      bsCollapse.hide();
-    });
+  collapseElements.forEach(function (element) {
+    let bsCollapse = new bootstrap.Collapse(element, { toggle: false });
+    bsCollapse.hide();
+  });
 
-    showElements.forEach(function(element) {
-      var bsShow = new bootstrap.Collapse(element, {toggle: false});
-      bsShow.show();
-    });
+  showElements.forEach(function (element) {
+    let bsShow = new bootstrap.Collapse(element, { toggle: false });
+    bsShow.show();
+  });
 }
 
-// Shows all elements with the ".multi-collapse" selector
+// Shows all elements with the ".appgroup" and ".multi-collapse" selector
 function showAll() {
-    var collapseElements = document.querySelectorAll('.multi-collapse');
+  const appGroups = document.querySelectorAll(".appgroup");
+  const collapseElements = document.querySelectorAll(".multi-collapse");
 
-    collapseElements.forEach(function(element) {
-        var bsCollapse = new bootstrap.Collapse(element, {toggle: false});
-        bsCollapse.show();
-    });
+  // Show all app group headers
+  appGroups.forEach(function (appGroup) {
+    let appGroupCollapse = new bootstrap.Collapse(appGroup, { toggle: false });
+    appGroupCollapse.show();
+  });
+
+  // Show all the apps
+  collapseElements.forEach(function (element) {
+    let bsCollapse = new bootstrap.Collapse(element, { toggle: false });
+    bsCollapse.show();
+  });
 }
+
+// Checks if all ".multi-collapse" children of an ".appgroup" are collapsed
+function allChildrenAreCollapsed(appGroup) {
+  const collapseElements = appGroup.querySelectorAll(".multi-collapse");
+  collapseElements.forEach((element) => {
+    if (element.classList.contains("show")) {
+      return false; // At least one child is not collapsed
+    }
+  });
+  return true; // All children are collapsed
+}
+
+// Handle collapsing/showing the "appgroup" headers
+function handleAppGroups() {
+  const appGroups = document.querySelectorAll(".appgroup");
+  appGroups.forEach((appGroup) => {
+    const groupCollapse = new bootstrap.Collapse(appGroup, { toggle: false });
+    if (allChildrenAreCollapsed(appGroup)) {
+      groupCollapse.hide();
+    } else {
+      groupCollapse.show();
+    }
+  });
+}
+
+// Register event listeners to all elements with the "multi-collapse" class
+document.addEventListener("DOMContentLoaded", function () {
+  const collapseElements = document.querySelectorAll(".multi-collapse");
+  collapseElements.forEach((element) => {
+    element.addEventListener("shown.bs.collapse", handleAppGroups);
+    element.addEventListener("hidden.bs.collapse", handleAppGroups);
+  });
+});
