770eb4c27a76d92ead6459df12b7754d8e1d7b7c
Molly Smith <molly.b.smith@noaa.gov>
2023-11-21 14:12:17 -0700

    DMC partial sums plots now available

--- apps/cb-metar/server/dataFunctions/data_dailymodelcycle.js
+++ apps/cb-metar/server/dataFunctions/data_dailymodelcycle.js
@@ -66,26 +66,25 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
       name: "variable",
     }).valuesMap;
     const queryVariable = Object.keys(variableValuesMap).filter(
-      (qv) =>
-        variableValuesMap[qv][0]
-          .map(function (v) {
-            return Object.keys(v)[0];
-          })
-          .indexOf(variable) === 0
+      (qv) => Object.keys(variableValuesMap[qv][0]).indexOf(variable) !== -1
     )[0];
+    const variableDetails = variableValuesMap[queryVariable][0][variable];
     const model = matsCollections["data-source"].findOne({ name: "data-source" })
       .optionsMap[variable][curve["data-source"]][0];
 
     const thresholdStr = curve.threshold;
-    let threshold = Object.keys(
-      matsCollections.threshold.findOne({ name: "threshold" }).valuesMap[variable]
-    ).find(
-      (key) =>
-        matsCollections.threshold.findOne({ name: "threshold" }).valuesMap[variable][
-          key
-        ] === thresholdStr
-    );
-    threshold = threshold.replace(/_/g, ".");
+    let threshold = "";
+    if (thresholdStr) {
+      threshold = Object.keys(
+        matsCollections.threshold.findOne({ name: "threshold" }).valuesMap[variable]
+      ).find(
+        (key) =>
+          matsCollections.threshold.findOne({ name: "threshold" }).valuesMap[variable][
+            key
+          ] === thresholdStr
+      );
+      threshold = threshold.replace(/_/g, ".");
+    }
 
     if (curve["utc-cycle-start"].length !== 1) {
       throw new Error(
@@ -100,6 +99,7 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
       { name: "statistic" },
       { optionsMap: 1 }
     ).optionsMap;
+    [statType] = statisticOptionsMap[variable][statisticSelect];
 
     let queryTemplate;
     let sitesList;
@@ -115,6 +115,7 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
       );
 
       // SQL template replacements
+      let statTemplate;
       queryTemplate = Assets.getText("sqlTemplates/tmpl_DailyModelCycle.sql");
       queryTemplate = queryTemplate.replace(/{{vxMODEL}}/g, model);
       queryTemplate = queryTemplate.replace(/{{vxREGION}}/g, region);
@@ -124,7 +125,17 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
         /{{vxVARIABLE}}/g,
         queryVariable.toUpperCase()
       );
-      queryTemplate = queryTemplate.replace(/{{vxTHRESHOLD}}/g, threshold);
+      if (statType === "ctc") {
+        statTemplate = Assets.getText("sqlTemplates/tmpl_CTC.sql");
+        queryTemplate = queryTemplate.replace(/{{vxSTATISTIC}}/g, statTemplate);
+        queryTemplate = queryTemplate.replace(/{{vxTHRESHOLD}}/g, threshold);
+        queryTemplate = queryTemplate.replace(/{{vxTYPE}}/g, "CTC");
+      } else {
+        statTemplate = Assets.getText("sqlTemplates/tmpl_PartialSums.sql");
+        queryTemplate = queryTemplate.replace(/{{vxSTATISTIC}}/g, statTemplate);
+        queryTemplate = queryTemplate.replace(/{{vxSUBVARIABLE}}/g, variableDetails[0]);
+        queryTemplate = queryTemplate.replace(/{{vxTYPE}}/g, "SUMS");
+      }
       queryTemplate = queryTemplate.replace(
         /{{vxUTC_CYCLE_START}}/g,
         cbPool.trfmListToCSVString(utcCycleStart, null, false)
@@ -142,8 +153,10 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
     // This axisKeySet object is used like a set and if a curve has the same
     // units (axisKey) it will use the same axis.
     // The axis number is assigned to the axisKeySet value, which is the axisKey.
-    [statType] = statisticOptionsMap[variable][statisticSelect];
-    const axisKey = statisticOptionsMap[variable][statisticSelect][1];
+    const axisKey =
+      statisticOptionsMap[variable][statisticSelect][1] === "Unknown"
+        ? variableDetails[1]
+        : statisticOptionsMap[variable][statisticSelect][1];
     curves[curveIndex].axisKey = axisKey; // stash the axisKey to use it later for axis options
     const idealVal = statisticOptionsMap[variable][statisticSelect][2];
     if (idealVal !== null && idealValues.indexOf(idealVal) === -1) {
@@ -178,7 +191,7 @@ dataDailyModelCycle = function (plotParams, plotFunction) {
           cbPool,
           regionType === "Predefined region" ? statement : rows,
           appParams,
-          statisticSelect
+          statType === "ctc" ? statisticSelect : `${statisticSelect}_${variable}`
         );
 
         finishMoment = moment();
