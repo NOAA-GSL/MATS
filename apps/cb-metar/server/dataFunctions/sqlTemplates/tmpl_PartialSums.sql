74c782d8e60c0c716318f189986398c4070aabc3
Molly Smith <molly.b.smith@noaa.gov>
2023-12-12 14:00:24 -0700

    Fixed bug where couchbase TO_STRING function did not work on NULLs

--- apps/cb-metar/server/dataFunctions/sqlTemplates/tmpl_PartialSums.sql
+++ apps/cb-metar/server/dataFunctions/sqlTemplates/tmpl_PartialSums.sql
@@ -4,5 +4,11 @@
        SUM(m0.data.['{{vxSUBVARIABLE}}'].sum_model) model_sum,
        SUM(m0.data.['{{vxSUBVARIABLE}}'].sum_obs) obs_sum,
        SUM(m0.data.['{{vxSUBVARIABLE}}'].sum_abs) abs_sum,
-       ARRAY_SORT( ARRAY_AGG( TO_STRING(m0.fcstValidEpoch) || ';' || TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum2_diff) || ';' || TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].num_recs) || ';' || TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum_diff * -1) || ';' || TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum_model) || ';' || TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum_obs) || ';' || TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum_abs) ) ) sub_data,
+       ARRAY_SORT( ARRAY_AGG( CASE WHEN m0.fcstValidEpoch IS NOT NULL THEN TO_STRING(m0.fcstValidEpoch) ELSE "NULL" END 
+       || ';' || CASE WHEN m0.data.['{{vxSUBVARIABLE}}'].sum2_diff IS NOT NULL THEN TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum2_diff) ELSE "NULL" END 
+       || ';' || CASE WHEN m0.data.['{{vxSUBVARIABLE}}'].num_recs IS NOT NULL THEN TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].num_recs) ELSE "NULL" END 
+       || ';' || CASE WHEN m0.data.['{{vxSUBVARIABLE}}'].sum_diff IS NOT NULL THEN TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum_diff * -1) ELSE "NULL" END 
+       || ';' || CASE WHEN m0.data.['{{vxSUBVARIABLE}}'].sum_model IS NOT NULL THEN TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum_model) ELSE "NULL" END 
+       || ';' || CASE WHEN m0.data.['{{vxSUBVARIABLE}}'].sum_obs IS NOT NULL THEN TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum_obs) ELSE "NULL" END 
+       || ';' || CASE WHEN m0.data.['{{vxSUBVARIABLE}}'].sum_abs IS NOT NULL THEN TO_STRING(m0.data.['{{vxSUBVARIABLE}}'].sum_abs) ELSE "NULL" END  ) ) sub_data,
        COUNT(m0.data.['{{vxSUBVARIABLE}}'].sum2_diff) N0
