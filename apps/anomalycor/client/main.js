a649421b532aa8c9bbafe95d02ad4478bf4cd328
Molly Smith <molly.b.smith@noaa.gov>
2024-01-10 13:12:34 -0700

    Removed anomalycor app

--- apps/anomalycor/client/main.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2021 Colorado State University and Regents of the University of Colorado. All rights reserved.
- */
-
-// eslint-disable-next-line no-unused-vars
-import { matsTypes, matsCollections, methods } from "meteor/randyp:mats-common";
-import "@fortawesome/fontawesome-free";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "@fortawesome/fontawesome-free/js/all.js";
