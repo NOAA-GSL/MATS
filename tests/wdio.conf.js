404b6039f0ab136022600438f6386cc61bfae79e
Ian McGinnis <67600557+ian-noaa@users.noreply.github.com>
2025-03-31 10:26:05 -0600

    Ask wdio to always use a stable release
    Webdriver had issues downloading the chromedriver version it desired
    this morning - it was getting a 404. Asking it to download the latest
    'stable' release fixes it.

--- tests/wdio.conf.js
+++ tests/wdio.conf.js
@@ -56,34 +56,32 @@ export const config = {
     // https://docs.saucelabs.com/reference/platforms-configurator
     //
     capabilities: debug ? [{
+        // Debug config
 
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
         // grid with only 5 firefox instances available you can make sure that not more than
         // 5 instances get started at a time.
         maxInstances: 1,
-        //
+
         browserName: 'chrome',
+        browserVersion: 'stable',
         'goog:chromeOptions': {
-            args: headless ? ['--headless', '--disable-gpu'] : [],
+            args: headless ? ['headless', 'disable-gpu'] : [],
         },
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
     }] : [{
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
+        // Normal config
+        
         maxInstances: 3,
-        //
+        
         browserName: 'chrome',
+        browserVersion: 'stable',
         'goog:chromeOptions': {
-            args: headless ? ['--headless', '--disable-gpu'] : [],
+            args: headless ? ['headless', 'disable-gpu'] : [],
         },
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
     }],
     //
     // ===================
